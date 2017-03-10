de1f9bf5e0e3be55f899d6c39ef7af7b9dfde4e1
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-03-10 15:06:19 -0500

    Clean up unused header files

--- src/shmemi/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SHMEMI_TIMER_H
-#define _SHMEMI_TIMER_H 1
-
-void shmemi_timer_init(void);
-void shmemi_timer_finalize(void);
-long shmemi_timer_get_elapsed_us(void);
-
-#endif /* ! _SHMEMI_TIMER_H 1 */
