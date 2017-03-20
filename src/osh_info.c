8e56621f27bb33e89c4757a44afd3da64972c331
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-03-20 15:19:39 -0400

    More tidying up of osh_info: make checks more robust

--- src/osh_info.c
+++ src/osh_info.c
@@ -8,6 +8,10 @@
 #include <unistd.h>
 #include <sys/param.h>
 
+#ifdef HAVE_UNAME
+# include <sys/utsname.h>
+#endif /* HAVE_UNAME */
+
 #include "version.h"
 
 static const int tag_width = 20;
@@ -17,6 +21,43 @@ void
 output(const char *tag, const char *val)
 {
     printf("%*s: %s\n", tag_width, tag, val);
+}
+
+static
+void
+output_package(void)
+{
+    output("Package name",
+#ifdef PACKAGE_NAME
+           PACKAGE_NAME
+#else
+           "unknown"
+#endif /* PACKAGE_NAME */
+           );
+
+    output("Package version",
+#ifdef PACKAGE_VERSION
+            PACKAGE_VERSION
+#else
+           "unknown"
+#endif /* PACKAGE_VERSION */
+           );
+
+    output("Package URL",
+#ifdef PACKAGE_URL
+            PACKAGE_URL
+#else
+           "unknown"
+#endif /* PACKAGE_URL */
+           );
+
+    output("Package bug report",
+#ifdef PACKAGE_BUGREPORT
+           PACKAGE_BUGREPORT
+#else
+           "unknown"
+#endif /* PACKAGE_BUGREPORT */
+           );
 }
 
 static
@@ -40,51 +81,46 @@ static
 void
 output_build_env(void)
 {
-    int s;
     const time_t t = time(NULL);
     char *now;
-    char host[MAXHOSTNAMELEN];
 
     now = ctime(&t);
     if (now != NULL) {
         now[strlen(now) - 1] = '\0'; /* chomp */
-        output("Build date", now);
+    } else {
+        now = "unknown";
     }
 
-    s = gethostname(host, MAXHOSTNAMELEN);
-    if (s == 0) {
-        output("Build host", host);
-    }
+    output("Build date", now);
 
+#ifdef HAVE_GETHOSTNAME
+    {
+        char host[MAXHOSTNAMELEN];
+        int s = gethostname(host, MAXHOSTNAMELEN);
+
+        output("Build host", (s == 0) ? host : "unknown");
+    }
+#elif defined(HAVE_UNAME)
+    {
+        struct utsname u;
+        int s = uname(&u);
+
+        output("Build host", (s == 0) ? u.nodename : "unknown");
+    }
+#else
+    output("Build host", "unknown");
+#endif /* hostname check */
+
+    /* command-line that built the library */
 #ifdef CONFIG_FLAGS
     output("Configure", CONFIG_FLAGS);
 #endif /* CONFIG_FLAGS */
 }
 
-int
-main(void)
+static
+void
+output_features(void)
 {
-
-#ifdef PACKAGE_NAME
-    output("Package name", PACKAGE_NAME);
-#endif /* PACKAGE_NAME */
-
-#ifdef PACKAGE_VERSION
-    output("Package version", PACKAGE_VERSION);
-#endif /* PACKAGE_VERSION */
-
-#ifdef PACKAGE_URL
-    output("Package URL", PACKAGE_URL);
-#endif /* PACKAGE_URL */
-
-#ifdef PACKAGE_BUGREPORT
-    output("Package bug report", PACKAGE_BUGREPORT);
-#endif /* PACKAGE_BUGREPORT */
-
-    output_spec_version();
-
-    output_build_env();
-
     output("Run debugging",
 #ifdef ENABLE_DEBUG
            "on"
@@ -124,6 +160,18 @@ main(void)
            "off"
 #endif /* ENABLE_FORTRAN */
            );
+}
+
+int
+main(void)
+{
+    output_package();
+
+    output_spec_version();
+
+    output_build_env();
+
+    output_features();
 
     return 0;
 }
