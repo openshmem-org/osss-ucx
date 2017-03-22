e0b3dafeaae13368f077634cc79c22df661e6041
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-03-22 10:49:27 -0400

    Move implementation to header as macros (skip one call layer)

--- src/shmemi/pe.c
+++ src/shmemi/pe.c
@@ -4,31 +4,3 @@
  * shared state
  */
 pe_t p;
-
-int
-shmem_my_pe(void)
-{
-    return p.me;
-}
-
-int
-shmem_n_pes(void)
-{
-    return p.npes;
-}
-
-/*
- * deprecated
- */
-
-int
-_my_pe(void)
-{
-    return p.me;
-}
-
-int
-_num_pes(void)
-{
-    return p.npes;
-}
