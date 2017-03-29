9f26f639d40f80553a065380c9c6c47e46ae457d
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-03-29 15:12:43 -0400

    Split out some functionality like header macros for internal use
    Move comms library up.  Fill out autoconf output files.

--- src/shmemc/shmemc.h
+++ src/shmemc/shmemc.h
@@ -1,6 +1,8 @@
 #ifndef _SHMEMC_COMMS_H
 #define _SHMEMC_COMMS_H 1
 
+#include <sys/types.h>
+
 void shmemc_quiet(void);
 void shmemc_fence(void);
 
@@ -10,12 +12,20 @@ void shmemc_fence(void);
 
 #include <stdlib.h>
 
-static
-inline
-char *
+static inline char *
 shmemc_getenv(const char *name)
 {
     return getenv(name);
+}
+
+/*
+ * aligned (or not) puts
+ */
+
+static inline void
+shmemc_put(void *dest, const void *src, size_t nelems, int pe)
+{
+    return;
 }
 
 #endif /* ! _SHMEMC_COMMS_H */
