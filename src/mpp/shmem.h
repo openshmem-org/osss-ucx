7d3078e745cf66a928d18453065ca509d4abd424
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-03-30 10:27:42 -0400

    Make the warning message a single string (safer that way)

--- src/mpp/shmem.h
+++ src/mpp/shmem.h
@@ -1,7 +1,7 @@
 #ifndef _MPP_SHMEM_H
 #define _MPP_SHMEM_H 1
 
-#warning <mpp/shmem.h> is a deprecated location, use <shmem.h> instead
+#warning "<mpp/shmem.h> is a deprecated location, use <shmem.h> instead"
 
 #include <shmem.h>
 
--- src/mpp/shmemx.h
+++ src/mpp/shmemx.h
@@ -1,7 +1,7 @@
 #ifndef _MPP_SHMEMX_H
 #define _MPP_SHMEMX_H 1
 
-#warning <mpp/shmemx.h> is a deprecated location, use <shmemx.h> instead
+#warning "<mpp/shmemx.h> is a deprecated location, use <shmemx.h> instead"
 
 #include <shmemx.h>
 
