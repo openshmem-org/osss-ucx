117a802d203e302600d036c4942eaa3f6e85d10f
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-18 13:14:41 -0400

    Add stubs for collect routines

---
+++ src/fcollect.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+
+/**
+ * Collective concatenation of 32-bit data from participating PEs
+ * into a target array on all those PEs
+ */
+
+void
+shmem_fcollect32(void *target, const void *source, size_t nelems,
+                 int PE_start, int logPE_stride, int PE_size,
+                 long *pSync)
+{
+}
+
+
+/**
+ * Collective concatenation of 64-bit data from participating PEs
+ * into a target array on all those PEs
+ */
+
+void
+shmem_fcollect64(void *target, const void *source, size_t nelems,
+                 int PE_start, int logPE_stride, int PE_size,
+                 long *pSync)
+{
+}
