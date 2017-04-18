5960c6de623494e6ff786aa620a0e2de396129f6
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-18 10:54:48 -0400

    Add stubs for wait routines

--- src/shmemc/shmemc.h
+++ src/shmemc/shmemc.h
@@ -1,5 +1,5 @@
-#ifndef _SHMEMC_COMMS_H
-#define _SHMEMC_COMMS_H 1
+#ifndef _SHMEMC_H
+#define _SHMEMC_H 1
 
 #include <sys/types.h>
 
@@ -47,12 +47,65 @@ void shmemc_iget(void *dest, const void *src, size_t nbytes,
                  ptrdiff_t tst, ptrdiff_t sst, int pe);
 
 /*
- * locks
+ * atomics and locks
  */
 
 void shmemc_set_lock(volatile long *lock);
 void shmemc_clear_lock(volatile long *lock);
 int  shmemc_test_lock(volatile long *lock);
+
+/*
+ * to be zapped
+ */
+#if 0
+#define VOLATILIZE(_type, _var) (* ( volatile _type *) (_var))
+#else
+#define VOLATILIZE(_type, _var) (_var)
+#endif
+
+/*
+ * routine per-type and test to avoid branching
+ *
+ * NEEDS PUSH-DOWN INTO COMMS IMPLEMENTATION FOR ACTUAL WAIT
+ */
+#define SHMEMC_WAITUNTIL_TYPE(_name, _type, _opname, _op)               \
+    inline static void                                                  \
+    shmemc_##_name##_wait_##_opname##_until(volatile _type *var,        \
+                                            int cmp,                    \
+                                            _type cmp_value)            \
+    {                                                                   \
+        do { } while ( *var _op cmp_value );                            \
+    }
+
+SHMEMC_WAITUNTIL_TYPE(short, short, eq, ==)
+SHMEMC_WAITUNTIL_TYPE(int, int, eq, ==)
+SHMEMC_WAITUNTIL_TYPE(long, long, eq, ==)
+SHMEMC_WAITUNTIL_TYPE(longlong, long long, eq, ==)
+
+SHMEMC_WAITUNTIL_TYPE(short, short, ne, !=)
+SHMEMC_WAITUNTIL_TYPE(int, int, ne, !=)
+SHMEMC_WAITUNTIL_TYPE(long, long, ne, !=)
+SHMEMC_WAITUNTIL_TYPE(longlong, long long, ne, !=)
+
+SHMEMC_WAITUNTIL_TYPE(short, short, gt, >)
+SHMEMC_WAITUNTIL_TYPE(int, int, gt, >)
+SHMEMC_WAITUNTIL_TYPE(long, long, gt, >)
+SHMEMC_WAITUNTIL_TYPE(longlong, long long, gt, >)
+
+SHMEMC_WAITUNTIL_TYPE(short, short, le, <=)
+SHMEMC_WAITUNTIL_TYPE(int, int, le, <=)
+SHMEMC_WAITUNTIL_TYPE(long, long, le, <=)
+SHMEMC_WAITUNTIL_TYPE(longlong, long long, le, <=)
+
+SHMEMC_WAITUNTIL_TYPE(short, short, lt, <)
+SHMEMC_WAITUNTIL_TYPE(int, int, lt, <)
+SHMEMC_WAITUNTIL_TYPE(long, long, lt, <)
+SHMEMC_WAITUNTIL_TYPE(longlong, long long, lt, <)
+
+SHMEMC_WAITUNTIL_TYPE(short, short, ge, >=)
+SHMEMC_WAITUNTIL_TYPE(int, int, ge, >=)
+SHMEMC_WAITUNTIL_TYPE(long, long, ge, >=)
+SHMEMC_WAITUNTIL_TYPE(longlong, long long, ge, >=)
 
 /*
  * barriers
@@ -71,4 +124,4 @@ shmemc_barrier(int start, int log_stride, int size, long *pSync)
 {
 }
 
-#endif /* ! _SHMEMC_COMMS_H */
+#endif /* ! _SHMEMC__H */
---
+++ src/waituntil.c
@@ -0,0 +1,63 @@
+#include "shmem/defs.h"
+#include "shmemi/shmemi.h"
+#include "shmemc/shmemc.h"
+
+/**
+ * wait_until with operator dispatchers, type-parameterized.
+ */
+#define SHMEM_TYPE_WAIT_UNTIL(_name, _type)                             \
+    void                                                                \
+    shmem_##_name##_wait_until(volatile _type *ivar, int cmp, _type cmp_value) \
+    {                                                                   \
+        switch (cmp) {                                                  \
+        case SHMEM_CMP_EQ:                                              \
+            shmemc_##_name##_wait_eq_until(ivar, cmp, cmp_value);       \
+            break;                                                      \
+        case SHMEM_CMP_NE:                                              \
+            shmemc_##_name##_wait_ne_until(ivar, cmp, cmp_value);       \
+            break;                                                      \
+        case SHMEM_CMP_GT:                                              \
+            shmemc_##_name##_wait_gt_until(ivar, cmp, cmp_value);       \
+            break;                                                      \
+        case SHMEM_CMP_LE:                                              \
+            shmemc_##_name##_wait_le_until(ivar, cmp, cmp_value);       \
+            break;                                                      \
+        case SHMEM_CMP_LT:                                              \
+            shmemc_##_name##_wait_lt_until(ivar, cmp, cmp_value);       \
+            break;                                                      \
+        case SHMEM_CMP_GE:                                              \
+            shmemc_##_name##_wait_ge_until(ivar, cmp, cmp_value);       \
+            break;                                                      \
+        default:                                                        \
+            logger(LOG_FATAL,                                           \
+                   "unknown operator (code %d) in"                      \
+                   __func__,                                            \
+                   cmp,                                                 \
+                   #_name                                               \
+                   );                                                   \
+            return;                                                     \
+            /* NOT REACHED */                                           \
+            break;                                                      \
+        }                                                               \
+    }
+
+SHMEM_TYPE_WAIT_UNTIL(short, short)
+SHMEM_TYPE_WAIT_UNTIL(int, int)
+SHMEM_TYPE_WAIT_UNTIL(long, long)
+SHMEM_TYPE_WAIT_UNTIL(longlong, long long)
+
+/**
+ * wait is just wait_until with inequality/change test
+ */
+
+#define SHMEM_TYPE_WAIT(_name, _type)                                 \
+    void                                                              \
+    shmem_##_name##_wait(volatile _type *ivar, _type cmp_value)       \
+    {                                                                 \
+        shmemc_##_name##_wait_ne_until(ivar, SHMEM_CMP_NE, cmp_value);  \
+    }
+
+SHMEM_TYPE_WAIT(short, short)
+SHMEM_TYPE_WAIT(int, int)
+SHMEM_TYPE_WAIT(long, long)
+SHMEM_TYPE_WAIT(longlong, long long)
