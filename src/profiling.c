037f53548e1871a04473c1a47cb6b7867839daf4
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-19 12:32:46 -0400

    Add pshmem control (proposed, only included if pshmem enabled)

---
+++ src/profiling.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "shmemi/shmemi.h"
+
+/**
+ * stub for the proposed UFL profiling (PSHMEM) interface
+ *
+ */
+
+void
+shmem_pcontrol(int level)
+{
+    char *msg = NULL;
+
+    switch (level) {
+    case 0:
+        msg = "disabled";
+        break;
+    case 1:
+        msg = "enabled (default detail)";
+        break;
+    default:
+        msg = "tool-specific";
+        break;
+    }
+
+    logger(LOG_INFO, "shmem_pcontrol(%d) is %s", level, msg);
+}
