9c91926ef1b4cedd07632a7525e2a0ca90b32170
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-20 16:59:35 -0400

    Add pshmem

--- src/broadcast.c
+++ src/broadcast.c
@@ -1,4 +1,17 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <sys/types.h>
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_broadcast32 = pshmem_broadcast32
+#define shmem_broadcast32 pshmem_broadcast32
+
+#pragma weak shmem_broadcast64 = pshmem_broadcast64
+#define shmem_broadcast64 pshmem_broadcast64
+#endif /* ENABLE_PSHMEM */
+
 
 void
 shmem_broadcast32(void *target, const void *source, size_t nelems,
