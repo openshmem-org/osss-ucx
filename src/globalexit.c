c23a2afe3138643ec9dbe67002ebae76675c93c6
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-20 17:04:46 -0400

    Add pshmem

--- src/globalexit.c
+++ src/globalexit.c
@@ -1,4 +1,18 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmemc/shmemc.h"
+
+/*
+ * These are compatibility routines for older SGI architectures.  They
+ * are now defined in OpenSHMEM to do nothing.
+ */
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_global_exit = pshmem_global_exit
+#define shmem_global_exit pshmem_global_exit
+#endif /* ENABLE_PSHMEM */
 
 void
 shmem_global_exit(int status)
