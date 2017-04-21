24433bd699f72cd40531dbd845c5c50e17478128
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-20 20:28:49 -0400

    Add pshmem

--- src/put-strided.c
+++ src/put-strided.c
@@ -1,6 +1,9 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmem/defs.h"
 #include "shmem/api.h"
-#include "shmemc/shmemc.h"
 
 /*
  * these are needed for propagating into Fortran,
@@ -11,6 +14,31 @@ extern void shmem_complexd_p(complex double *addr, complex double value, int pe)
 extern void shmem_char_iput(char *target, const char *source, ptrdiff_t tst,
                             ptrdiff_t sst, size_t nelems, int pe);
 
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_short_iput = pshmem_short_iput
+#define shmem_short_iput pshmem_short_iput
+#pragma weak shmem_char_iput = pshmem_char_iput
+#define shmem_char_iput pshmem_char_iput
+#pragma weak shmem_int_iput = pshmem_int_iput
+#define shmem_int_iput pshmem_int_iput
+#pragma weak shmem_long_iput = pshmem_long_iput
+#define shmem_long_iput pshmem_long_iput
+#pragma weak shmem_double_iput = pshmem_double_iput
+#define shmem_double_iput pshmem_double_iput
+#pragma weak shmem_float_iput = pshmem_float_iput
+#define shmem_float_iput pshmem_float_iput
+#pragma weak shmem_longdouble_iput = pshmem_longdouble_iput
+#define shmem_longdouble_iput pshmem_longdouble_iput
+#pragma weak shmem_longlong_iput = pshmem_longlong_iput
+#define shmem_longlong_iput pshmem_longlong_iput
+#pragma weak shmem_iput32 = pshmem_iput32
+#define shmem_iput32 pshmem_iput32
+#pragma weak shmem_iput64 = pshmem_iput64
+#define shmem_iput64 pshmem_iput64
+#pragma weak shmem_iput128 = pshmem_iput128
+#define shmem_iput128 pshmem_iput128
+#endif /* ENABLE_PSHMEM */
+
 #define SHMEM_EMIT_IPUT(_name, _type)                                   \
     void                                                                \
     shmem_##_name##_iput(_type *target, const _type *source,            \
@@ -19,7 +47,7 @@ extern void shmem_char_iput(char *target, const char *source, ptrdiff_t tst,
         size_t ti = 0, si = 0;                                          \
         size_t i;                                                       \
         for (i = 0; i < nelems; i += 1) {                               \
-            shmem_##_name##_p(& (target[ti]), source[si], pe);          \
+            shmem_##_name##_p(& (target[ti]), source[si], pe);         \
             ti += tst;                                                  \
             si += sst;                                                  \
         }                                                               \
@@ -42,7 +70,7 @@ SHMEM_EMIT_IPUT(complexd, COMPLEXIFY(double))
                       ptrdiff_t tst, ptrdiff_t sst, size_t nelems, int pe) \
     {                                                                   \
         const size_t sized_nelems = nelems * _size;                     \
-        shmemc_iput(dest, src, tst, sst, sized_nelems, pe);             \
+        shmem_iput##_size(dest, src, tst, sst, sized_nelems, pe);       \
     }
 
 SHMEM_SIZED_IPUT(32, 32)
