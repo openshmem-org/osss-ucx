8214a9919e9465fff0d5d65ba71c3fab7cc8587e
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 18:56:33 -0400

    Add pshmem

--- src/put-nbi.c
+++ src/put-nbi.c
@@ -1,5 +1,48 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmem/defs.h"
 #include "shmemc/shmemc.h"
+
+#ifdef ENABLE_PSHMEM
+extern void shmem_complexf_put_nbi(COMPLEXIFY (float) * dest,
+                                   const COMPLEXIFY (float) * src,
+                                   size_t nelems, int pe); /* ! API */
+extern void shmem_complexd_put_nbi(COMPLEXIFY (double) * dest,
+                                   const COMPLEXIFY (double) * src,
+                                   size_t nelems, int pe); /* ! API */
+#pragma weak shmem_short_put_nbi = pshmem_short_put_nbi
+#define shmem_short_put_nbi pshmem_short_put_nbi
+#pragma weak shmem_int_put_nbi = pshmem_int_put_nbi
+#define shmem_int_put_nbi pshmem_int_put_nbi
+#pragma weak shmem_char_put_nbi = pshmem_char_put_nbi
+#define shmem_char_put_nbi pshmem_char_put_nbi
+#pragma weak shmem_long_put_nbi = pshmem_long_put_nbi
+#define shmem_long_put_nbi pshmem_long_put_nbi
+#pragma weak shmem_longdouble_put_nbi = pshmem_longdouble_put_nbi
+#define shmem_longdouble_put_nbi pshmem_longdouble_put_nbi
+#pragma weak shmem_longlong_put_nbi = pshmem_longlong_put_nbi
+#define shmem_longlong_put_nbi pshmem_longlong_put_nbi
+#pragma weak shmem_double_put_nbi = pshmem_double_put_nbi
+#define shmem_double_put_nbi pshmem_double_put_nbi
+#pragma weak shmem_float_put_nbi = pshmem_float_put_nbi
+#define shmem_float_put_nbi pshmem_float_put_nbi
+#pragma weak shmem_complexf_put_nbi = pshmem_complexf_put_nbi
+#define shmem_complexf_put_nbi pshmem_complexf_put_nbi
+#pragma weak shmem_complexd_put_nbi = pshmem_complexd_put_nbi
+#define shmem_complexd_put_nbi pshmem_complexd_put_nbi
+#pragma weak shmem_putmem_nbi = pshmem_putmem_nbi
+#define shmem_putmem_nbi pshmem_putmem_nbi
+#pragma weak shmem_put32_nbi = pshmem_put32_nbi
+#define shmem_put32_nbi pshmem_put32_nbi
+#pragma weak shmem_put64_nbi = pshmem_put64_nbi
+#define shmem_put64_nbi pshmem_put64_nbi
+#pragma weak shmem_put128_nbi = pshmem_put128_nbi
+#define shmem_put128_nbi pshmem_put128_nbi
+/* # pragma weak pshmem_put_nbi = pshmem_long_put_nbi */
+/* # pragma weak shmem_put_nbi = pshmem_put_nbi */
+#endif /* ENABLE_PSHMEM */
 
 #define SHMEM_TYPED_PUT_NBI(_name, _type)                               \
     void                                                                \
