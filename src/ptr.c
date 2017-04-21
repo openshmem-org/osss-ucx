89f693eb859b8c6a8d2d1ec34df2385c2725659c
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-20 20:28:19 -0400

    Add pshmem

--- src/ptr.c
+++ src/ptr.c
@@ -1,8 +1,17 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <stdio.h>
 
 /*
  * boringly implemented stub
  */
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_ptr = pshmem_ptr
+#define shmem_ptr pshmem_ptr
+#endif /* ENABLE_PSHMEM */
 
 void *
 shmem_ptr(const void *target, int pe)
