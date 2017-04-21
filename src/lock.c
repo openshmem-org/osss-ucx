83531fbc2927266ee22423b26034dc7d6d8620a9
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-20 20:28:31 -0400

    Add pshmem

--- src/lock.c
+++ src/lock.c
@@ -6,7 +6,20 @@
  *
  */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmemc/shmemc.h"
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_set_lock = pshmem_set_lock
+#define shmem_set_lock pshmem_set_lock
+#pragma weak shmem_test_lock = pshmem_test_lock
+#define shmem_test_lock pshmem_test_lock
+#pragma weak shmem_clear_lock = pshmem_clear_lock
+#define shmem_clear_lock pshmem_clear_lock
+#endif /* ENABLE_PSHMEM */
 
 void
 shmem_set_lock(volatile long *lock)
