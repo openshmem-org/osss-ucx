6f5addc73dbf8d8139e175925b338f53f48f2bd3
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 19:00:49 -0400

    Add pshmem

--- src/alltoall.c
+++ src/alltoall.c
@@ -1,5 +1,23 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <sys/types.h>
 #include <stddef.h>
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_alltoall32 = pshmem_alltoall32
+#define shmem_alltoall32 pshmem_alltoall32
+
+#pragma weak shmem_alltoall64 = pshmem_alltoall64
+#define shmem_alltoall64 pshmem_alltoall64
+
+#pragma weak shmem_alltoalls32 = pshmem_alltoalls32
+#define shmem_alltoalls32 pshmem_alltoalls32
+
+#pragma weak shmem_alltoalls64 = pshmem_alltoalls64
+#define shmem_alltoalls64 pshmem_alltoalls64
+#endif /* ENABLE_PSHMEM */
 
 void
 shmem_alltoall32(void *target, const void* source, size_t nelems,
