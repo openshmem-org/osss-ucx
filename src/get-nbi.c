5dbc76e621167ee0dc5d5b7ce6be8dd0c265bde6
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 18:57:50 -0400

    Add pshmem

--- src/get-nbi.c
+++ src/get-nbi.c
@@ -1,5 +1,49 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmem/defs.h"
 #include "shmemc/shmemc.h"
+
+#ifdef ENABLE_PSHMEM
+extern void shmem_complexf_get_nbi(COMPLEXIFY (float) * dest,
+                                   const COMPLEXIFY (float) * src,
+                                   size_t nelems, int pe); /* ! API */
+extern void shmem_complexd_get_nbi(COMPLEXIFY (double) * dest,
+                                   const COMPLEXIFY (double) * src,
+                                   size_t nelems, int pe); /* ! API */
+#pragma weak shmem_short_get_nbi = pshmem_short_get_nbi
+#define shmem_short_get_nbi pshmem_short_get_nbi
+#pragma weak shmem_int_get_nbi = pshmem_int_get_nbi
+#define shmem_int_get_nbi pshmem_int_get_nbi
+#pragma weak shmem_char_get_nbi = pshmem_char_get_nbi
+#define shmem_char_get_nbi pshmem_char_get_nbi
+#pragma weak shmem_long_get_nbi = pshmem_long_get_nbi
+#define shmem_long_get_nbi pshmem_long_get_nbi
+#pragma weak shmem_longdouble_get_nbi = pshmem_longdouble_get_nbi
+#define shmem_longdouble_get_nbi pshmem_longdouble_get_nbi
+#pragma weak shmem_longlong_get_nbi = pshmem_longlong_get_nbi
+#define shmem_longlong_get_nbi pshmem_longlong_get_nbi
+#pragma weak shmem_double_get_nbi = pshmem_double_get_nbi
+#define shmem_double_get_nbi pshmem_double_get_nbi
+#pragma weak shmem_float_get_nbi = pshmem_float_get_nbi
+#define shmem_float_get_nbi pshmem_float_get_nbi
+#pragma weak shmem_complexf_get_nbi = pshmem_complexf_get_nbi
+#define shmem_complexf_get_nbi pshmem_complexf_get_nbi
+#pragma weak shmem_complexd_get_nbi = pshmem_complexd_get_nbi
+#define shmem_complexd_get_nbi pshmem_complexd_get_nbi
+#pragma weak shmem_getmem_nbi = pshmem_getmem_nbi
+#define shmem_getmem_nbi pshmem_getmem_nbi
+#pragma weak shmem_get32_nbi = pshmem_get32_nbi
+#define shmem_get32_nbi pshmem_get32_nbi
+#pragma weak shmem_get64_nbi = pshmem_get64_nbi
+#define shmem_get64_nbi pshmem_get64_nbi
+#pragma weak shmem_get128_nbi = pshmem_get128_nbi
+#define shmem_get128_nbi pshmem_get128_nbi
+/* # pragma weak pshmem_get_nbi = pshmem_long_get_nbi */
+/* # pragma weak shmem_get_nbi = pshmem_get_nbi */
+#endif /* ENABLE_PSHMEM */
+
 
 #define SHMEM_TYPED_GET_NBI(_name, _type)                               \
     void                                                                \
