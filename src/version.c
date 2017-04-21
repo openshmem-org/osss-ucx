a0cc28bee40a0379ecd09c0ee0a9bfae7d53fb07
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 19:25:44 -0400

    Add pshmem

--- src/version.c
+++ src/version.c
@@ -1,6 +1,17 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <string.h>
 
 #include "version.h"
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_info_get_version = pshmem_info_get_version
+#define shmem_info_get_version pshmem_info_get_version
+#pragma weak shmem_info_get_name = pshmem_info_get_name
+#define shmem_info_get_name pshmem_info_get_name
+#endif /* ENABLE_PSHMEM */
 
 void
 shmem_info_get_version(int *major, int *minor)
