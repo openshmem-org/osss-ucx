1ba337d5cd9f2a106bdfbe69f329169595222100
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 18:54:58 -0400

    Add pshmem

--- src/get-strided.c
+++ src/get-strided.c
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmem/defs.h"
 #include "shmem/api.h"
 #include "shmemc/shmemc.h"
@@ -10,6 +14,31 @@ extern complex float shmem_complexf_g(complex float *addr, int pe);
 extern complex double shmem_complexd_g(complex double *addr, int pe);
 extern void shmem_char_iget(char *target, const char *source, ptrdiff_t tst,
                             ptrdiff_t sst, size_t nelems, int pe);
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_char_iget = pshmem_char_iget
+#define shmem_char_iget pshmem_char_iget
+#pragma weak shmem_short_iget = pshmem_short_iget
+#define shmem_short_iget pshmem_short_iget
+#pragma weak shmem_int_iget = pshmem_int_iget
+#define shmem_int_iget pshmem_int_iget
+#pragma weak shmem_long_iget = pshmem_long_iget
+#define shmem_long_iget pshmem_long_iget
+#pragma weak shmem_double_iget = pshmem_double_iget
+#define shmem_double_iget pshmem_double_iget
+#pragma weak shmem_float_iget = pshmem_float_iget
+#define shmem_float_iget pshmem_float_iget
+#pragma weak shmem_longdouble_iget = pshmem_longdouble_iget
+#define shmem_longdouble_iget pshmem_longdouble_iget
+#pragma weak shmem_longlong_iget = pshmem_longlong_iget
+#define shmem_longlong_iget pshmem_longlong_iget
+#pragma weak shmem_iget32 = pshmem_iget32
+#define shmem_iget32 pshmem_iget32
+#pragma weak shmem_iget64 = pshmem_iget64
+#define shmem_iget64 pshmem_iget64
+#pragma weak shmem_iget128 = pshmem_iget128
+#define shmem_iget128 pshmem_iget128
+#endif /* ENABLE_PSHMEM */
 
 #define SHMEM_EMIT_IGET(_name, _type)                                   \
     void                                                                \
