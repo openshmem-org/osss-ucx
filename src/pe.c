71e88c10d33ea3f0fc4d55363a9de4174681569e
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 19:22:42 -0400

    Add pshmem

--- src/pe.c
+++ src/pe.c
@@ -1,4 +1,20 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "shmemi/shmemi.h"
+
+#ifdef ENABLE_PSHMEM
+#pragma weak _my_pe = p_my_pe
+#define _my_pe p_my_pe
+#pragma weak shmem_my_pe = pshmem_my_pe
+#define shmem_my_pe pshmem_my_pe
+
+#pragma weak _num_pes = p_num_pes
+#define _num_pes p_num_pes
+#pragma weak shmem_n_pes = pshmem_n_pes
+#define shmem_n_pes pshmem_n_pes
+#endif /* ENABLE_PSHMEM */
 
 int
 shmem_my_pe(void)
@@ -15,11 +31,13 @@ shmem_n_pes(void)
 int
 _my_pe(void)
 {
+    deprecate(__func__);
     return shmemi_my_pe();
 }
 
 int
 _num_pes(void)
 {
+    deprecate(__func__);
     return shmemi_n_pes();
 }
