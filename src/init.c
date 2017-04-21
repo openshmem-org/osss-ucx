83c414040fc73d5615870df33c693ab51e33608f
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-20 20:29:31 -0400

    Add pshmem

--- src/init.c
+++ src/init.c
@@ -1,15 +1,31 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "shmemi/shmemi.h"
 #include "shmemu/shmemu.h"
+#include "shmemc/shmemc.h"
+#include "shmemi/shmemi.h"
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_init = pshmem_init
+#define shmem_init pshmem_init
+#pragma weak shmem_finalize = pshmem_finalize
+#define shmem_finalize pshmem_finalize
+
+#pragma weak start_pes = pstart_pes
+#define start_pes pstart_pes
+#endif /* ENABLE_PSHMEM */
 
 void
 shmem_init(void)
 {
     shmemu_init();
+    shmemc_init();
     shmemi_init();
 
     /* urgh! */
@@ -19,6 +35,7 @@ void
 shmem_finalize(void)
 {
     shmemi_finalize();
+    shmemc_finalize();
     shmemu_finalize();
 }
 
