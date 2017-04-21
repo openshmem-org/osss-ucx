cebc9dc7fcd514cb3f4415d7813151f1c3cbdd63
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-04-21 18:59:12 -0400

    Add pshmem

--- src/fcollect.c
+++ src/fcollect.c
@@ -1,4 +1,17 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include <sys/types.h>
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_fcollect32 = pshmem_fcollect32
+#define shmem_fcollect32 pshmem_fcollect32
+
+#pragma weak shmem_fcollect64 = pshmem_fcollect64
+#define shmem_fcollect64 pshmem_fcollect64
+#endif /* ENABLE_PSHMEM */
+
 
 /**
  * Collective concatenation of 32-bit data from participating PEs
