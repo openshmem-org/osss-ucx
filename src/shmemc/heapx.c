f3712f1b647cba46b8addd81b6f039fa628b997e
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-07-19 14:41:04 -0400

    Fix heap types

--- src/shmemc/heapx.c
+++ src/shmemc/heapx.c
@@ -1,32 +1,20 @@
+#include "heapx.h"
+#include "thispe.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <assert.h>
-
-#include "thispe.h"
 
 void
 heapx_init(void)
 {
     if (proc.heaps == NULL) {
-        int pe;
+        fprintf(stderr, "%d: ranks = %d\n", proc.rank, proc.nranks);
 
         /* allocate heap stubs for all ranks */
-        proc.heaps = (heapx_t *) calloc(proc.nranks, sizeof(heapx_t));
+        proc.heaps = (heapx_t *) calloc(proc.nranks,
+                                      sizeof(heapx_t));
         assert(proc.heaps != NULL);
-
-        for (pe = 0; pe < proc.nranks; pe += 1) {
-            int h;
-
-            /* foreach rank, allocate symmetric heap stubs */
-            proc.heaps[pe] =
-                (symmetric_heaps_t *) calloc(proc.nheaps,
-                                             sizeof(symmetric_heaps_t));
-            for (h = 0; h < proc.nheaps; h += 1) {
-                /* present, but uninitialized */
-                proc.heaps[pe][h]->base = NULL;
-            }
-        }
     }
 }
 
@@ -34,18 +22,6 @@ void
 heapx_finalize(void)
 {
     if (proc.heaps != NULL) {
-        int pe;
-
-        for (pe = 0; pe < proc.nranks; pe += 1) {
-            int h;
-
-            for (h = 0; h < proc.nheaps; h += 1) {
-                free(proc.heaps[pe][h]);
-            }
-
-            free(proc.heaps[pe]);
-        }
-
         free(proc.heaps);
     }
 }
--- src/shmemc/heapx.h
+++ src/shmemc/heapx.h
@@ -7,19 +7,12 @@
 
 #include <sys/types.h>
 
+/*
+ * each PE has a symmetric heap, with a start address, and size.
+ */
 typedef struct symmetric_heap {
-    void *base;                 /* start address */
-    size_t size;                /* bytes in this heap */
-} symmetric_heap_t;
-
-/*
- * each PE has nheaps symmetric heaps
- */
-typedef symmetric_heap_t *symmetric_heaps_t;
-
-/*
- * each PE records nranks-1 exchange info of all PEs
- */
-typedef symmetric_heaps_t *heapx_t;
+    void *base;
+    size_t size;
+} heapx_t;
 
 #endif /* ! _HEAPX_H */
