8a7ff620dabf3250ca4872249d441bc0f21b504d
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-09 11:15:28 -0400

    Remove old memory region defs

--- src/shmemc/state.c
+++ src/shmemc/state.c
@@ -8,18 +8,4 @@
 thispe_info_t proc = {
     .status = SHMEM_PE_UNKNOWN,
     .refcount = 0,
-    .heaps = NULL
 };
-
-/*
- * where globals/statics/commons/saves live
- *
- */
-memory_region_t global_segment;
-
-/*
- * some memory to play with registering
- *
- * TODO: multiple heap support
- */
-memory_region_t symm_segment;
