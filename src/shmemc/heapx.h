a760f653f772612bc65e9e11755ca980b30075f0
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-09 11:14:25 -0400

    Utility references to memory segments

--- src/shmemc/heapx.h
+++ src/shmemc/heapx.h
@@ -7,17 +7,7 @@
 
 #include <sys/types.h>
 
-#include <ucp/api/ucp.h>
-
-/*
- * each PE has a symmetric heap, with a start address, and size.
- */
-typedef struct heapx {
-    void *base;                 /* start of this region */
-    size_t length;              /* its size (b) */
-} heapx_t;
-
-void shmemc_heapx_init(void);
-void shmemc_heapx_finalize(void);
+extern heapx_t *global_segment;
+extern heapx_t *symm_segment;
 
 #endif /* ! _HEAPX_H */
