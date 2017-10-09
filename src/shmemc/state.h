66465ba9b7ef57ce21ef0fcff412951d1c3084c2
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-09 11:15:49 -0400

    Remove old memory refs

--- src/shmemc/state.h
+++ src/shmemc/state.h
@@ -3,11 +3,6 @@
 
 #include "thispe.h"
 
-#include <ucp/api/ucp.h>
-
 extern thispe_info_t proc;
-
-extern memory_region_t global_segment;
-extern memory_region_t symm_segment;
 
 #endif /* ! _SHMEMC_STATE_H */
