9ee34aa619952765985ef2bb82ddc6300d790008
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-23 16:16:44 -0400

    No more global barrier init

--- src/shmemc/shmemc-init.c
+++ src/shmemc/shmemc-init.c
@@ -1,31 +1,9 @@
 #include "shmemc-ucx.h"
 #include "pmix-client.h"
 #include "state.h"
+#include "shmemu.h"
 #include "shmemc.h"
 #include "shmem/defs.h"
-
-long *shmemc_all_sync;
-
-inline static void
-barrier_all_init(void)
-{
-    int i;
-
-    shmemc_all_sync =
-        (long *) shmemc_mem_alloc(SHMEM_BARRIER_SYNC_SIZE *
-                                  sizeof(*shmemc_all_sync));
-    for (i = 0; i < SHMEM_BARRIER_SYNC_SIZE; i += 1) {
-        shmemc_all_sync[i] = SHMEM_SYNC_VALUE;
-    }
-}
-
-inline static void
-barrier_all_finalize(void)
-{
-    if (shmemc_all_sync != NULL) {
-        shmemc_mem_free(shmemc_all_sync);
-    }
-}
 
 void
 shmemc_init(void)
@@ -49,19 +27,17 @@ shmemc_init(void)
     shmemc_pmix_publish_my_rkeys();
     shmemc_pmix_exchange_all_rkeys();
 
-    barrier_all_init();
-
     proc.status = SHMEM_PE_RUNNING;
 }
 
 void
 shmemc_finalize(void)
 {
+    shmemc_barrier_all();
+
     shmemc_ucx_finalize();
 
     shmemc_pmix_client_finalize();
-
-    barrier_all_finalize();
 
     proc.status = SHMEM_PE_SHUTDOWN;
 }
