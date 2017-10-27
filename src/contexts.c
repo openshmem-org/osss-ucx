cf9117d98f6435cbc603f1214046d9c6bd49751c
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-27 12:52:56 -0400

    Trivial contexts implementation
    Needs to be fleshed out as index into array of context state

--- src/contexts.c
+++ src/contexts.c
@@ -4,26 +4,25 @@
 # include "config.h"
 #endif /* HAVE_CONFIG_H */
 
-#include "shmemu.h"
 #include "shmemc.h"
 #include "shmem/defs.h"
 
+#include <stdlib.h>
+
 #ifdef ENABLE_PSHMEM
-#pragma weak shmem_short_wait_until = pshmem_short_wait_until
-#define shmem_short_wait_until pshmem_short_wait_until
-#pragma weak shmem_int_wait_until = pshmem_int_wait_until
-#define shmem_int_wait_until pshmem_int_wait_until
-#pragma weak shmem_long_wait_until = pshmem_long_wait_until
-#define shmem_long_wait_until pshmem_long_wait_until
-#pragma weak shmem_longlong_wait_until = pshmem_longlong_wait_until
-#define shmem_longlong_wait_until pshmem_longlong_wait_until
+#pragma weak shmem_ctx_create = pshmem_ctx_create
+#define shmem_ctx_create pshmem_ctx_create
+#pragma weak shmem_ctx_destroy = pshmem_ctx_destroy
+#define shmem_ctx_destroy pshmem_ctx_destroy
 #endif /* ENABLE_PSHMEM */
 
 
 int
 shmem_ctx_create(long options, shmem_ctx_t *ctxp)
 {
-    return 0;
+    *ctxp = 1;
+
+    return 1;
 }
 
 void
