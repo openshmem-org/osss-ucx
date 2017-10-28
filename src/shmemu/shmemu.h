115ebaed58eaf948ca3a05c5b97b180c010d5b22
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-28 15:08:59 -0400

    Complete movement of reduction ops to separate source

--- src/shmemu/shmemu.h
+++ src/shmemu/shmemu.h
@@ -60,10 +60,85 @@ void shmemu_logger(shmemu_log_t level, const char *fmt, ...);
 # define logger(...)
 #endif /* ENABLE_DEBUG */
 
+/*
+ * need to restrict this report to first usage only
+ */
 inline static void
 deprecate(const char *fn)
 {
     logger(LOG_DEPRECATE, "\"%s\" is deprecated", fn);
 }
+
+#include <sys/types.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <complex.h>
+
+#define SHMEMU_DECL_MATH_FUNC(_name, _type)                 \
+    _type shmemu_sum_##_name##_func(_type _a, _type _b);    \
+    _type shmemu_prod_##_name##_func(_type _a, _type _b);
+
+SHMEMU_DECL_MATH_FUNC(float, float)
+SHMEMU_DECL_MATH_FUNC(double, double)
+SHMEMU_DECL_MATH_FUNC(short, short)
+SHMEMU_DECL_MATH_FUNC(int, int)
+SHMEMU_DECL_MATH_FUNC(long, long)
+SHMEMU_DECL_MATH_FUNC(longlong, long long)
+SHMEMU_DECL_MATH_FUNC(longdouble, long double)
+SHMEMU_DECL_MATH_FUNC(uint, unsigned int)
+SHMEMU_DECL_MATH_FUNC(ulong, unsigned long)
+SHMEMU_DECL_MATH_FUNC(ulonglong, unsigned long long)
+SHMEMU_DECL_MATH_FUNC(int32, int32_t)
+SHMEMU_DECL_MATH_FUNC(int64, int64_t)
+SHMEMU_DECL_MATH_FUNC(uint32, uint32_t)
+SHMEMU_DECL_MATH_FUNC(uint64, uint64_t)
+SHMEMU_DECL_MATH_FUNC(complexd, double complex)
+SHMEMU_DECL_MATH_FUNC(complexf, float complex)
+
+/**
+ * these are the logical operations.  Note: these are *bitwise*.
+ *
+ */
+
+#define SHMEMU_DECL_LOGIC_FUNC(_name, _type)                \
+    _type shmemu_and_##_name##_func(_type _a, _type _b);    \
+    _type shmemu_or_##_name##_func(_type _a, _type _b);     \
+    _type shmemu_xor_##_name##_func(_type _a, _type _b);
+
+SHMEMU_DECL_LOGIC_FUNC(short, short)
+SHMEMU_DECL_LOGIC_FUNC(int, int)
+SHMEMU_DECL_LOGIC_FUNC(long, long)
+SHMEMU_DECL_LOGIC_FUNC(longlong, long long)
+SHMEMU_DECL_LOGIC_FUNC(uint, unsigned int)
+SHMEMU_DECL_LOGIC_FUNC(ulong, unsigned long)
+SHMEMU_DECL_LOGIC_FUNC(ulonglong, unsigned long long)
+SHMEMU_DECL_LOGIC_FUNC(int32, int32_t)
+SHMEMU_DECL_LOGIC_FUNC(int64, int64_t)
+SHMEMU_DECL_LOGIC_FUNC(uint32, uint32_t)
+SHMEMU_DECL_LOGIC_FUNC(uint64, uint64_t)
+
+/**
+ * these are the minima/maxima operations
+ *
+ */
+
+#define SHMEMU_DECL_MINIMAX_FUNC(_name, _type)              \
+    _type shmemu_min_##_name##_func(_type _a, _type _b);    \
+    _type shmemu_max_##_name##_func(_type _a, _type _b);
+
+SHMEMU_DECL_MINIMAX_FUNC(short, short)
+SHMEMU_DECL_MINIMAX_FUNC(int, int)
+SHMEMU_DECL_MINIMAX_FUNC(long, long)
+SHMEMU_DECL_MINIMAX_FUNC(longlong, long long)
+SHMEMU_DECL_MINIMAX_FUNC(float, float)
+SHMEMU_DECL_MINIMAX_FUNC(double, double)
+SHMEMU_DECL_MINIMAX_FUNC(longdouble, long double)
+SHMEMU_DECL_MINIMAX_FUNC(uint, unsigned int)
+SHMEMU_DECL_MINIMAX_FUNC(ulong, unsigned long)
+SHMEMU_DECL_MINIMAX_FUNC(ulonglong, unsigned long long)
+SHMEMU_DECL_MINIMAX_FUNC(int32, int32_t)
+SHMEMU_DECL_MINIMAX_FUNC(int64, int64_t)
+SHMEMU_DECL_MINIMAX_FUNC(uint32, uint32_t)
+SHMEMU_DECL_MINIMAX_FUNC(uint64, uint64_t)
 
 #endif /* ! _SHMEM_SHEMU_H */
