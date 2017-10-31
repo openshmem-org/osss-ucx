46e149c2bbe70132475e7d8a908af8dc71625e72
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-31 12:13:34 -0400

    strided puts and gets types/sizes/pshmem

--- src/get-strided.c
+++ src/get-strided.c
@@ -21,22 +21,55 @@ extern void shmem_complexd_get(COMPLEXIFY(double) *dest,
                                int pe);
 
 #ifdef ENABLE_PSHMEM
+#pragma weak shmem_float_iget = pshmem_float_iget
+#define shmem_float_iget pshmem_float_iget
+#pragma weak shmem_double_iget = pshmem_double_iget
+#define shmem_double_iget pshmem_double_iget
+#pragma weak shmem_longdouble_iget = pshmem_longdouble_iget
+#define shmem_longdouble_iget pshmem_longdouble_iget
 #pragma weak shmem_char_iget = pshmem_char_iget
 #define shmem_char_iget pshmem_char_iget
+#pragma weak shmem_schar_iget = pshmem_schar_iget
+#define shmem_schar_iget pshmem_schar_iget
 #pragma weak shmem_short_iget = pshmem_short_iget
 #define shmem_short_iget pshmem_short_iget
 #pragma weak shmem_int_iget = pshmem_int_iget
 #define shmem_int_iget pshmem_int_iget
 #pragma weak shmem_long_iget = pshmem_long_iget
 #define shmem_long_iget pshmem_long_iget
-#pragma weak shmem_double_iget = pshmem_double_iget
-#define shmem_double_iget pshmem_double_iget
-#pragma weak shmem_float_iget = pshmem_float_iget
-#define shmem_float_iget pshmem_float_iget
-#pragma weak shmem_longdouble_iget = pshmem_longdouble_iget
-#define shmem_longdouble_iget pshmem_longdouble_iget
 #pragma weak shmem_longlong_iget = pshmem_longlong_iget
 #define shmem_longlong_iget pshmem_longlong_iget
+#pragma weak shmem_uchar_iget = pshmem_uchar_iget
+#define shmem_uchar_iget pshmem_uchar_iget
+#pragma weak shmem_ushort_iget = pshmem_ushort_iget
+#define shmem_ushort_iget pshmem_ushort_iget
+#pragma weak shmem_uint_iget = pshmem_uint_iget
+#define shmem_uint_iget pshmem_uint_iget
+#pragma weak shmem_ulong_iget = pshmem_ulong_iget
+#define shmem_ulong_iget pshmem_ulong_iget
+#pragma weak shmem_ulonglong_iget = pshmem_ulonglong_iget
+#define shmem_ulonglong_iget pshmem_ulonglong_iget
+#pragma weak shmem_int8_iget = pshmem_int8_iget
+#define shmem_int8_iget pshmem_int8_iget
+#pragma weak shmem_int16_iget = pshmem_int16_iget
+#define shmem_int16_iget pshmem_int16_iget
+#pragma weak shmem_int32_iget = pshmem_int32_iget
+#define shmem_int32_iget pshmem_int32_iget
+#pragma weak shmem_int64_iget = pshmem_int64_iget
+#define shmem_int64_iget pshmem_int64_iget
+#pragma weak shmem_uint8_iget = pshmem_uint8_iget
+#define shmem_uint8_iget pshmem_uint8_iget
+#pragma weak shmem_uint16_iget = pshmem_uint16_iget
+#define shmem_uint16_iget pshmem_uint16_iget
+#pragma weak shmem_uint32_iget = pshmem_uint32_iget
+#define shmem_uint32_iget pshmem_uint32_iget
+#pragma weak shmem_uint64_iget = pshmem_uint64_iget
+#define shmem_uint64_iget pshmem_uint64_iget
+
+#pragma weak shmem_iget8 = pshmem_iget8
+#define shmem_iget8 pshmem_iget8
+#pragma weak shmem_iget16 = pshmem_iget16
+#define shmem_iget16 pshmem_iget16
 #pragma weak shmem_iget32 = pshmem_iget32
 #define shmem_iget32 pshmem_iget32
 #pragma weak shmem_iget64 = pshmem_iget64
@@ -45,7 +78,7 @@ extern void shmem_complexd_get(COMPLEXIFY(double) *dest,
 #define shmem_iget128 pshmem_iget128
 #endif /* ENABLE_PSHMEM */
 
-#define SHMEM_EMIT_IGET(_name, _type)                                   \
+#define SHMEM_TYPED_IGET(_name, _type)                                  \
     void                                                                \
     shmem_##_name##_iget(_type *target, const _type *source,            \
                          ptrdiff_t tst, ptrdiff_t sst,                  \
@@ -61,19 +94,35 @@ extern void shmem_complexd_get(COMPLEXIFY(double) *dest,
         }                                                               \
     }
 
-SHMEM_EMIT_IGET(char, char)
-SHMEM_EMIT_IGET(short, short)
-SHMEM_EMIT_IGET(int, int)
-SHMEM_EMIT_IGET(long, long)
-SHMEM_EMIT_IGET(float, float)
-SHMEM_EMIT_IGET(double, double)
-SHMEM_EMIT_IGET(longlong, long long)
-SHMEM_EMIT_IGET(longdouble, long double)
+SHMEM_TYPED_IGET(float, float)
+SHMEM_TYPED_IGET(double, double)
+SHMEM_TYPED_IGET(longdouble, long double)
+SHMEM_TYPED_IGET(char, char)
+SHMEM_TYPED_IGET(schar, signed char)
+SHMEM_TYPED_IGET(short, short)
+SHMEM_TYPED_IGET(int, int)
+SHMEM_TYPED_IGET(long, long)
+SHMEM_TYPED_IGET(longlong, long long)
+SHMEM_TYPED_IGET(uchar, unsigned char)
+SHMEM_TYPED_IGET(ushort, unsigned short)
+SHMEM_TYPED_IGET(uint, unsigned int)
+SHMEM_TYPED_IGET(ulong, unsigned long)
+SHMEM_TYPED_IGET(ulonglong, unsigned long long)
+SHMEM_TYPED_IGET(int8, int8_t)
+SHMEM_TYPED_IGET(int16, int16_t)
+SHMEM_TYPED_IGET(int32, int32_t)
+SHMEM_TYPED_IGET(int64, int64_t)
+SHMEM_TYPED_IGET(uint8, uint8_t)
+SHMEM_TYPED_IGET(uint16, uint16_t)
+SHMEM_TYPED_IGET(uint32, uint32_t)
+SHMEM_TYPED_IGET(uint64, uint64_t)
+SHMEM_TYPED_IGET(size, size_t)
+SHMEM_TYPED_IGET(ptrdiff, ptrdiff_t)
 /* for Fortran */
-SHMEM_EMIT_IGET(complexf, COMPLEXIFY(float))
-SHMEM_EMIT_IGET(complexd, COMPLEXIFY(double))
+SHMEM_TYPED_IGET(complexf, COMPLEXIFY(float))
+SHMEM_TYPED_IGET(complexd, COMPLEXIFY(double))
 
-#define SHMEM_SIZED_IGET(_name, _size)                                  \
+#define SHMEM_SIZED_IGET(_size)                                         \
     void                                                                \
     shmem_iget##_size(void *target, const void *source,                 \
                       ptrdiff_t tst, ptrdiff_t sst,                     \
@@ -83,20 +132,14 @@ SHMEM_EMIT_IGET(complexd, COMPLEXIFY(double))
         size_t i;                                                       \
                                                                         \
         for (i = 0; i < nelems; i += 1) {                               \
-            shmem_get##_name(target + ti, source + si, 1, pe);          \
+            shmem_get##_size(target + ti, source + si, 1, pe);          \
             ti += tst;                                                  \
             si += sst;                                                  \
         }                                                               \
     }
 
-SHMEM_SIZED_IGET(32, 32)
-SHMEM_SIZED_IGET(32, 4)
-#if __WORDSIZE == 64
-SHMEM_SIZED_IGET(64, 64)
-SHMEM_SIZED_IGET(64, 8)
-#else
-SHMEM_SIZED_IGET(32, 32)
-SHMEM_SIZED_IGET(32, 4)
-#endif
-/* not sure about 128 yet, fake for now */
-SHMEM_SIZED_IGET(128, 128)
+SHMEM_SIZED_IGET(8)
+SHMEM_SIZED_IGET(16)
+SHMEM_SIZED_IGET(32)
+SHMEM_SIZED_IGET(64)
+SHMEM_SIZED_IGET(128)
--- src/put-strided.c
+++ src/put-strided.c
@@ -21,22 +21,55 @@ extern void shmem_complexd_put(COMPLEXIFY(double) *dest,
                                int pe);
 
 #ifdef ENABLE_PSHMEM
-#pragma weak shmem_short_iput = pshmem_short_iput
-#define shmem_short_iput pshmem_short_iput
+#pragma weak shmem_float_iput = pshmem_float_iput
+#define shmem_float_iput pshmem_float_iput
+#pragma weak shmem_double_iput = pshmem_double_iput
+#define shmem_double_iput pshmem_double_iput
+#pragma weak shmem_longdouble_iput = pshmem_longdouble_iput
+#define shmem_longdouble_iput pshmem_longdouble_iput
 #pragma weak shmem_char_iput = pshmem_char_iput
 #define shmem_char_iput pshmem_char_iput
+#pragma weak shmem_schar_iput = pshmem_schar_iput
+#define shmem_schar_iput pshmem_schar_iput
+#pragma weak shmem_short_iput = pshmem_short_iput
+#define shmem_short_iput pshmem_short_iput
 #pragma weak shmem_int_iput = pshmem_int_iput
 #define shmem_int_iput pshmem_int_iput
 #pragma weak shmem_long_iput = pshmem_long_iput
 #define shmem_long_iput pshmem_long_iput
-#pragma weak shmem_double_iput = pshmem_double_iput
-#define shmem_double_iput pshmem_double_iput
-#pragma weak shmem_float_iput = pshmem_float_iput
-#define shmem_float_iput pshmem_float_iput
-#pragma weak shmem_longdouble_iput = pshmem_longdouble_iput
-#define shmem_longdouble_iput pshmem_longdouble_iput
 #pragma weak shmem_longlong_iput = pshmem_longlong_iput
 #define shmem_longlong_iput pshmem_longlong_iput
+#pragma weak shmem_uchar_iput = pshmem_uchar_iput
+#define shmem_uchar_iput pshmem_uchar_iput
+#pragma weak shmem_ushort_iput = pshmem_ushort_iput
+#define shmem_ushort_iput pshmem_ushort_iput
+#pragma weak shmem_uint_iput = pshmem_uint_iput
+#define shmem_uint_iput pshmem_uint_iput
+#pragma weak shmem_ulong_iput = pshmem_ulong_iput
+#define shmem_ulong_iput pshmem_ulong_iput
+#pragma weak shmem_ulonglong_iput = pshmem_ulonglong_iput
+#define shmem_ulonglong_iput pshmem_ulonglong_iput
+#pragma weak shmem_int8_iput = pshmem_int8_iput
+#define shmem_int8_iput pshmem_int8_iput
+#pragma weak shmem_int16_iput = pshmem_int16_iput
+#define shmem_int16_iput pshmem_int16_iput
+#pragma weak shmem_int32_iput = pshmem_int32_iput
+#define shmem_int32_iput pshmem_int32_iput
+#pragma weak shmem_int64_iput = pshmem_int64_iput
+#define shmem_int64_iput pshmem_int64_iput
+#pragma weak shmem_uint8_iput = pshmem_uint8_iput
+#define shmem_uint8_iput pshmem_uint8_iput
+#pragma weak shmem_uint16_iput = pshmem_uint16_iput
+#define shmem_uint16_iput pshmem_uint16_iput
+#pragma weak shmem_uint32_iput = pshmem_uint32_iput
+#define shmem_uint32_iput pshmem_uint32_iput
+#pragma weak shmem_uint64_iput = pshmem_uint64_iput
+#define shmem_uint64_iput pshmem_uint64_iput
+
+#pragma weak shmem_iput8 = pshmem_iput8
+#define shmem_iput8 pshmem_iput8
+#pragma weak shmem_iput16 = pshmem_iput16
+#define shmem_iput16 pshmem_iput16
 #pragma weak shmem_iput32 = pshmem_iput32
 #define shmem_iput32 pshmem_iput32
 #pragma weak shmem_iput64 = pshmem_iput64
@@ -45,9 +78,9 @@ extern void shmem_complexd_put(COMPLEXIFY(double) *dest,
 #define shmem_iput128 pshmem_iput128
 #endif /* ENABLE_PSHMEM */
 
-#define SHMEM_EMIT_IPUT(_name, _type)                                   \
+#define SHMEM_TYPED_IPUT(_name, _type)                                  \
     void                                                                \
-    shmem_##_name##_iput(_type *target, const _type *source,            \
+    shmem_##_name##_iput(_type *tarput, const _type *source,            \
                          ptrdiff_t tst, ptrdiff_t sst,                  \
                          size_t nelems, int pe)                         \
     {                                                                   \
@@ -55,25 +88,41 @@ extern void shmem_complexd_put(COMPLEXIFY(double) *dest,
         size_t i;                                                       \
                                                                         \
         for (i = 0; i < nelems; i += 1) {                               \
-            shmem_##_name##_put(target + ti, source + si, 1, pe);       \
+            shmem_##_name##_put(tarput + ti, source + si, 1, pe);       \
             ti += tst;                                                  \
             si += sst;                                                  \
         }                                                               \
     }
 
-SHMEM_EMIT_IPUT(char, char)
-SHMEM_EMIT_IPUT(short, short)
-SHMEM_EMIT_IPUT(int, int)
-SHMEM_EMIT_IPUT(long, long)
-SHMEM_EMIT_IPUT(float, float)
-SHMEM_EMIT_IPUT(double, double)
-SHMEM_EMIT_IPUT(longlong, long long)
-SHMEM_EMIT_IPUT(longdouble, long double)
+SHMEM_TYPED_IPUT(float, float)
+SHMEM_TYPED_IPUT(double, double)
+SHMEM_TYPED_IPUT(longdouble, long double)
+SHMEM_TYPED_IPUT(char, char)
+SHMEM_TYPED_IPUT(schar, signed char)
+SHMEM_TYPED_IPUT(short, short)
+SHMEM_TYPED_IPUT(int, int)
+SHMEM_TYPED_IPUT(long, long)
+SHMEM_TYPED_IPUT(longlong, long long)
+SHMEM_TYPED_IPUT(uchar, unsigned char)
+SHMEM_TYPED_IPUT(ushort, unsigned short)
+SHMEM_TYPED_IPUT(uint, unsigned int)
+SHMEM_TYPED_IPUT(ulong, unsigned long)
+SHMEM_TYPED_IPUT(ulonglong, unsigned long long)
+SHMEM_TYPED_IPUT(int8, int8_t)
+SHMEM_TYPED_IPUT(int16, int16_t)
+SHMEM_TYPED_IPUT(int32, int32_t)
+SHMEM_TYPED_IPUT(int64, int64_t)
+SHMEM_TYPED_IPUT(uint8, uint8_t)
+SHMEM_TYPED_IPUT(uint16, uint16_t)
+SHMEM_TYPED_IPUT(uint32, uint32_t)
+SHMEM_TYPED_IPUT(uint64, uint64_t)
+SHMEM_TYPED_IPUT(size, size_t)
+SHMEM_TYPED_IPUT(ptrdiff, ptrdiff_t)
 /* for Fortran */
-SHMEM_EMIT_IPUT(complexf, COMPLEXIFY(float))
-SHMEM_EMIT_IPUT(complexd, COMPLEXIFY(double))
+SHMEM_TYPED_IPUT(complexf, COMPLEXIFY(float))
+SHMEM_TYPED_IPUT(complexd, COMPLEXIFY(double))
 
-#define SHMEM_SIZED_IPUT(_name, _size)                                  \
+#define SHMEM_SIZED_IPUT(_size)                                         \
     void                                                                \
     shmem_iput##_size(void *target, const void *source,                 \
                       ptrdiff_t tst, ptrdiff_t sst,                     \
@@ -83,20 +132,14 @@ SHMEM_EMIT_IPUT(complexd, COMPLEXIFY(double))
         size_t i;                                                       \
                                                                         \
         for (i = 0; i < nelems; i += 1) {                               \
-            shmem_put##_name(target + ti, source + si, 1, pe);          \
+            shmem_put##_size(target + ti, source + si, 1, pe);          \
             ti += tst;                                                  \
             si += sst;                                                  \
         }                                                               \
     }
 
-SHMEM_SIZED_IPUT(32, 32)
-SHMEM_SIZED_IPUT(32, 4)
-#if __WORDSIZE == 64
-SHMEM_SIZED_IPUT(64, 64)
-SHMEM_SIZED_IPUT(64, 8)
-#else
-SHMEM_SIZED_IPUT(32, 32)
-SHMEM_SIZED_IPUT(32, 4)
-#endif
-/* not sure about 128 yet, fake for now */
-SHMEM_SIZED_IPUT(128, 128)
+SHMEM_SIZED_IPUT(8)
+SHMEM_SIZED_IPUT(16)
+SHMEM_SIZED_IPUT(32)
+SHMEM_SIZED_IPUT(64)
+SHMEM_SIZED_IPUT(128)
