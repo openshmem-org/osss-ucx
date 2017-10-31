9bc28ab10d8b53fb2864ad6d4668dead235d1bfe
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-10-31 10:52:58 -0400

    Add PSHMEM for all puts and gets

--- src/get.c
+++ src/get.c
@@ -11,36 +11,66 @@ extern void shmem_complexd_get(COMPLEXIFY(double) * dest,
                                const COMPLEXIFY(double) * src,
                                size_t nelems, int pe);    /* ! API */
 
+#pragma weak shmem_float_get = pshmem_float_get
+#define shmem_float_get pshmem_float_get
+#pragma weak shmem_double_get = pshmem_double_get
+#define shmem_double_get pshmem_double_get
+#pragma weak shmem_longdouble_get = pshmem_longdouble_get
+#define shmem_longdouble_get pshmem_longdouble_get
+#pragma weak shmem_char_get = pshmem_char_get
+#define shmem_char_get pshmem_char_get
+#pragma weak shmem_schar_get = pshmem_schar_get
+#define shmem_schar_get pshmem_schar_get
 #pragma weak shmem_short_get = pshmem_short_get
 #define shmem_short_get pshmem_short_get
 #pragma weak shmem_int_get = pshmem_int_get
 #define shmem_int_get pshmem_int_get
-#pragma weak shmem_char_get = pshmem_char_get
-#define shmem_char_get pshmem_char_get
 #pragma weak shmem_long_get = pshmem_long_get
 #define shmem_long_get pshmem_long_get
-#pragma weak shmem_longdouble_get = pshmem_longdouble_get
-#define shmem_longdouble_get pshmem_longdouble_get
 #pragma weak shmem_longlong_get = pshmem_longlong_get
 #define shmem_longlong_get pshmem_longlong_get
-#pragma weak shmem_double_get = pshmem_double_get
-#define shmem_double_get pshmem_double_get
-#pragma weak shmem_float_get = pshmem_float_get
-#define shmem_float_get pshmem_float_get
-#pragma weak shmem_complexf_get = pshmem_complexf_get
-#define shmem_complexf_get pshmem_complexf_get
-#pragma weak shmem_complexd_get = pshmem_complexd_get
-#define shmem_complexd_get pshmem_complexd_get
-#pragma weak shmem_getmem = pshmem_getmem
-#define shmem_getmem pshmem_getmem
+#pragma weak shmem_uchar_get = pshmem_uchar_get
+#define shmem_uchar_get pshmem_uchar_get
+#pragma weak shmem_ushort_get = pshmem_ushort_get
+#define shmem_ushort_get pshmem_ushort_get
+#pragma weak shmem_uint_get = pshmem_uint_get
+#define shmem_uint_get pshmem_uint_get
+#pragma weak shmem_ulong_get = pshmem_ulong_get
+#define shmem_ulong_get pshmem_ulong_get
+#pragma weak shmem_ulonglong_get = pshmem_ulonglong_get
+#define shmem_ulonglong_get pshmem_ulonglong_get
+#pragma weak shmem_int8_get = pshmem_int8_get
+#define shmem_int8_get pshmem_int8_get
+#pragma weak shmem_int16_get = pshmem_int16_get
+#define shmem_int16_get pshmem_int16_get
+#pragma weak shmem_int32_get = pshmem_int32_get
+#define shmem_int32_get pshmem_int32_get
+#pragma weak shmem_int64_get = pshmem_int64_get
+#define shmem_int64_get pshmem_int64_get
+#pragma weak shmem_uint8_get = pshmem_uint8_get
+#define shmem_uint8_get pshmem_uint8_get
+#pragma weak shmem_uint16_get = pshmem_uint16_get
+#define shmem_uint16_get pshmem_uint16_get
+#pragma weak shmem_uint32_get = pshmem_uint32_get
+#define shmem_uint32_get pshmem_uint32_get
+#pragma weak shmem_uint64_get = pshmem_uint64_get
+#define shmem_uint64_get pshmem_uint64_get
+
 #pragma weak shmem_get32 = pshmem_get32
 #define shmem_get32 pshmem_get32
 #pragma weak shmem_get64 = pshmem_get64
 #define shmem_get64 pshmem_get64
 #pragma weak shmem_get128 = pshmem_get128
 #define shmem_get128 pshmem_get128
-/* # pragma weak pshmem_get = pshmem_long_get */
-/* # pragma weak shmem_get = pshmem_get */
+
+#pragma weak shmem_getmem = pshmem_getmem
+#define shmem_getmem pshmem_getmem
+
+/* for Fortran */
+#pragma weak shmem_complexf_get = pshmem_complexf_get
+#define shmem_complexf_get pshmem_complexf_get
+#pragma weak shmem_complexd_get = pshmem_complexd_get
+#define shmem_complexd_get pshmem_complexd_get
 #endif /* ENABLE_PSHMEM */
 
 #define SHMEM_TYPED_GET(_name, _type)                            \
@@ -52,14 +82,31 @@ extern void shmem_complexd_get(COMPLEXIFY(double) * dest,
         shmemc_get(dest, src, typed_nelems, pe);                 \
     }
 
+SHMEM_TYPED_GET(float, float)
+SHMEM_TYPED_GET(double, double)
+SHMEM_TYPED_GET(longdouble, long double)
 SHMEM_TYPED_GET(char, char)
+SHMEM_TYPED_GET(schar, signed char)
 SHMEM_TYPED_GET(short, short)
 SHMEM_TYPED_GET(int, int)
 SHMEM_TYPED_GET(long, long)
-SHMEM_TYPED_GET(longdouble, long double)
 SHMEM_TYPED_GET(longlong, long long)
-SHMEM_TYPED_GET(double, double)
-SHMEM_TYPED_GET(float, float)
+SHMEM_TYPED_GET(uchar, unsigned char)
+SHMEM_TYPED_GET(ushort, unsigned short)
+SHMEM_TYPED_GET(uint, unsigned int)
+SHMEM_TYPED_GET(ulong, unsigned long)
+SHMEM_TYPED_GET(ulonglong, unsigned long long)
+SHMEM_TYPED_GET(int8, int8_t)
+SHMEM_TYPED_GET(int16, int16_t)
+SHMEM_TYPED_GET(int32, int32_t)
+SHMEM_TYPED_GET(int64, int64_t)
+SHMEM_TYPED_GET(uint8, uint8_t)
+SHMEM_TYPED_GET(uint16, uint16_t)
+SHMEM_TYPED_GET(uint32, uint32_t)
+SHMEM_TYPED_GET(uint64, uint64_t)
+SHMEM_TYPED_GET(size, size_t)
+SHMEM_TYPED_GET(ptrdiff, ptrdiff_t)
+/* for Fortran */
 SHMEM_TYPED_GET(complexf, COMPLEXIFY(float))
 SHMEM_TYPED_GET(complexd, COMPLEXIFY(double))
 
@@ -85,6 +132,58 @@ shmem_getmem(void *dest, const void *src,
     shmemc_get(dest, src, nelems, pe);
 }
 
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_float_g = pshmem_float_g
+#define shmem_float_g pshmem_float_g
+#pragma weak shmem_double_g = pshmem_double_g
+#define shmem_double_g pshmem_double_g
+#pragma weak shmem_longdouble_g = pshmem_longdouble_g
+#define shmem_longdouble_g pshmem_longdouble_g
+#pragma weak shmem_char_g = pshmem_char_g
+#define shmem_char_g pshmem_char_g
+#pragma weak shmem_schar_g = pshmem_schar_g
+#define shmem_schar_g pshmem_schar_g
+#pragma weak shmem_short_g = pshmem_short_g
+#define shmem_short_g pshmem_short_g
+#pragma weak shmem_int_g = pshmem_int_g
+#define shmem_int_g pshmem_int_g
+#pragma weak shmem_long_g = pshmem_long_g
+#define shmem_long_g pshmem_long_g
+#pragma weak shmem_longlong_g = pshmem_longlong_g
+#define shmem_longlong_g pshmem_longlong_g
+#pragma weak shmem_uchar_g = pshmem_uchar_g
+#define shmem_uchar_g pshmem_uchar_g
+#pragma weak shmem_ushort_g = pshmem_ushort_g
+#define shmem_ushort_g pshmem_ushort_g
+#pragma weak shmem_uint_g = pshmem_uint_g
+#define shmem_uint_g pshmem_uint_g
+#pragma weak shmem_ulong_g = pshmem_ulong_g
+#define shmem_ulong_g pshmem_ulong_g
+#pragma weak shmem_ulonglong_g = pshmem_ulonglong_g
+#define shmem_ulonglong_g pshmem_ulonglong_g
+#pragma weak shmem_int8_g = pshmem_int8_g
+#define shmem_int8_g pshmem_int8_g
+#pragma weak shmem_int16_g = pshmem_int16_g
+#define shmem_int16_g pshmem_int16_g
+#pragma weak shmem_int32_g = pshmem_int32_g
+#define shmem_int32_g pshmem_int32_g
+#pragma weak shmem_int64_g = pshmem_int64_g
+#define shmem_int64_g pshmem_int64_g
+#pragma weak shmem_uint8_g = pshmem_uint8_g
+#define shmem_uint8_g pshmem_uint8_g
+#pragma weak shmem_uint16_g = pshmem_uint16_g
+#define shmem_uint16_g pshmem_uint16_g
+#pragma weak shmem_uint32_g = pshmem_uint32_g
+#define shmem_uint32_g pshmem_uint32_g
+#pragma weak shmem_uint64_g = pshmem_uint64_g
+#define shmem_uint64_g pshmem_uint64_g
+/* for Fortran */
+#pragma weak shmem_complexf_g = pshmem_complexf_g
+#define shmem_complexf_g pshmem_complexf_g
+#pragma weak shmem_complexd_g = pshmem_complexd_g
+#define shmem_complexd_g pshmem_complexd_g
+#endif /* ENABLE_PSHMEM */
+
 #define SHMEM_TYPED_G_WRAPPER(_name, _type)                          \
     _type                                                            \
     shmem_##_name##_g(const _type *addr, int pe)                     \
@@ -94,13 +193,30 @@ shmem_getmem(void *dest, const void *src,
         return val;                                                  \
     }
 
+SHMEM_TYPED_G_WRAPPER(float, float)
+SHMEM_TYPED_G_WRAPPER(double, double)
+SHMEM_TYPED_G_WRAPPER(longdouble, long double)
 SHMEM_TYPED_G_WRAPPER(char, char)
+SHMEM_TYPED_G_WRAPPER(schar, signed char)
 SHMEM_TYPED_G_WRAPPER(short, short)
 SHMEM_TYPED_G_WRAPPER(int, int)
 SHMEM_TYPED_G_WRAPPER(long, long)
 SHMEM_TYPED_G_WRAPPER(longlong, long long)
-SHMEM_TYPED_G_WRAPPER(float, float)
-SHMEM_TYPED_G_WRAPPER(double, double)
-SHMEM_TYPED_G_WRAPPER(longdouble, long double)
+SHMEM_TYPED_G_WRAPPER(uchar, unsigned char)
+SHMEM_TYPED_G_WRAPPER(ushort, unsigned short)
+SHMEM_TYPED_G_WRAPPER(uint, unsigned int)
+SHMEM_TYPED_G_WRAPPER(ulong, unsigned long)
+SHMEM_TYPED_G_WRAPPER(ulonglong, unsigned long long)
+SHMEM_TYPED_G_WRAPPER(int8, int8_t)
+SHMEM_TYPED_G_WRAPPER(int16, int16_t)
+SHMEM_TYPED_G_WRAPPER(int32, int32_t)
+SHMEM_TYPED_G_WRAPPER(int64, int64_t)
+SHMEM_TYPED_G_WRAPPER(uint8, uint8_t)
+SHMEM_TYPED_G_WRAPPER(uint16, uint16_t)
+SHMEM_TYPED_G_WRAPPER(uint32, uint32_t)
+SHMEM_TYPED_G_WRAPPER(uint64, uint64_t)
+SHMEM_TYPED_G_WRAPPER(size, size_t)
+SHMEM_TYPED_G_WRAPPER(ptrdiff, ptrdiff_t)
+/* for Fortran */
 SHMEM_TYPED_G_WRAPPER(complexf, COMPLEXIFY(float))
 SHMEM_TYPED_G_WRAPPER(complexd, COMPLEXIFY(double))
--- src/put.c
+++ src/put.c
@@ -8,36 +8,67 @@
 #include "shmemc.h"
 
 #ifdef ENABLE_PSHMEM
+#pragma weak shmem_float_put = pshmem_float_put
+#define shmem_float_put pshmem_float_put
+#pragma weak shmem_double_put = pshmem_double_put
+#define shmem_double_put pshmem_double_put
+#pragma weak shmem_longdouble_put = pshmem_longdouble_put
+#define shmem_longdouble_put pshmem_longdouble_put
+#pragma weak shmem_char_put = pshmem_char_put
+#define shmem_char_put pshmem_char_put
+#pragma weak shmem_schar_put = pshmem_schar_put
+#define shmem_schar_put pshmem_schar_put
 #pragma weak shmem_short_put = pshmem_short_put
 #define shmem_short_put pshmem_short_put
 #pragma weak shmem_int_put = pshmem_int_put
 #define shmem_int_put pshmem_int_put
-#pragma weak shmem_char_put = pshmem_char_put
-#define shmem_char_put pshmem_char_put
 #pragma weak shmem_long_put = pshmem_long_put
 #define shmem_long_put pshmem_long_put
-#pragma weak shmem_longdouble_put = pshmem_longdouble_put
-#define shmem_longdouble_put pshmem_longdouble_put
 #pragma weak shmem_longlong_put = pshmem_longlong_put
 #define shmem_longlong_put pshmem_longlong_put
-#pragma weak shmem_double_put = pshmem_double_put
-#define shmem_double_put pshmem_double_put
-#pragma weak shmem_float_put = pshmem_float_put
-#define shmem_float_put pshmem_float_put
-#pragma weak shmem_complexf_put = pshmem_complexf_put
-#define shmem_complexf_put pshmem_complexf_put
-#pragma weak shmem_complexd_put = pshmem_complexd_put
-#define shmem_complexd_put pshmem_complexd_put
-#pragma weak shmem_putmem = pshmem_putmem
-#define shmem_putmem pshmem_putmem
+#pragma weak shmem_uchar_put = pshmem_uchar_put
+#define shmem_uchar_put pshmem_uchar_put
+#pragma weak shmem_ushort_put = pshmem_ushort_put
+#define shmem_ushort_put pshmem_ushort_put
+#pragma weak shmem_uint_put = pshmem_uint_put
+#define shmem_uint_put pshmem_uint_put
+#pragma weak shmem_ulong_put = pshmem_ulong_put
+#define shmem_ulong_put pshmem_ulong_put
+#pragma weak shmem_ulonglong_put = pshmem_ulonglong_put
+#define shmem_ulonglong_put pshmem_ulonglong_put
+#pragma weak shmem_int8_put = pshmem_int8_put
+#define shmem_int8_put pshmem_int8_put
+#pragma weak shmem_int16_put = pshmem_int16_put
+#define shmem_int16_put pshmem_int16_put
+#pragma weak shmem_int32_put = pshmem_int32_put
+#define shmem_int32_put pshmem_int32_put
+#pragma weak shmem_int64_put = pshmem_int64_put
+#define shmem_int64_put pshmem_int64_put
+#pragma weak shmem_uint8_put = pshmem_uint8_put
+#define shmem_uint8_put pshmem_uint8_put
+#pragma weak shmem_uint16_put = pshmem_uint16_put
+#define shmem_uint16_put pshmem_uint16_put
+#pragma weak shmem_uint32_put = pshmem_uint32_put
+#define shmem_uint32_put pshmem_uint32_put
+#pragma weak shmem_uint64_put = pshmem_uint64_put
+#define shmem_uint64_put pshmem_uint64_put
+
 #pragma weak shmem_put32 = pshmem_put32
 #define shmem_put32 pshmem_put32
 #pragma weak shmem_put64 = pshmem_put64
 #define shmem_put64 pshmem_put64
 #pragma weak shmem_put128 = pshmem_put128
 #define shmem_put128 pshmem_put128
-/* # pragma weak pshmem_put = pshmem_long_put */
-/* # pragma weak shmem_put = pshmem_put */
+
+#pragma weak shmem_putmem = pshmem_putmem
+#define shmem_putmem pshmem_putmem
+
+/* for Fortran */
+#pragma weak shmem_complexf_put = pshmem_complexf_put
+#define shmem_complexf_put pshmem_complexf_put
+#pragma weak shmem_complexd_put = pshmem_complexd_put
+#define shmem_complexd_put pshmem_complexd_put
+
 #endif /* ENABLE_PSHMEM */
 
 
@@ -50,14 +81,30 @@
         shmemc_put(dest, src, sized_nelems, pe);                        \
     }
 
+SHMEM_TYPED_PUT(float, float)
+SHMEM_TYPED_PUT(double, double)
+SHMEM_TYPED_PUT(longdouble, long double)
 SHMEM_TYPED_PUT(char, char)
+SHMEM_TYPED_PUT(schar, signed char)
 SHMEM_TYPED_PUT(short, short)
 SHMEM_TYPED_PUT(int, int)
 SHMEM_TYPED_PUT(long, long)
 SHMEM_TYPED_PUT(longlong, long long)
-SHMEM_TYPED_PUT(longdouble, long double)
-SHMEM_TYPED_PUT(double, double)
-SHMEM_TYPED_PUT(float, float)
+SHMEM_TYPED_PUT(uchar, unsigned char)
+SHMEM_TYPED_PUT(ushort, unsigned short)
+SHMEM_TYPED_PUT(uint, unsigned int)
+SHMEM_TYPED_PUT(ulong, unsigned long)
+SHMEM_TYPED_PUT(ulonglong, unsigned long long)
+SHMEM_TYPED_PUT(int8, int8_t)
+SHMEM_TYPED_PUT(int16, int16_t)
+SHMEM_TYPED_PUT(int32, int32_t)
+SHMEM_TYPED_PUT(int64, int64_t)
+SHMEM_TYPED_PUT(uint8, uint8_t)
+SHMEM_TYPED_PUT(uint16, uint16_t)
+SHMEM_TYPED_PUT(uint32, uint32_t)
+SHMEM_TYPED_PUT(uint64, uint64_t)
+SHMEM_TYPED_PUT(size, size_t)
+SHMEM_TYPED_PUT(ptrdiff, ptrdiff_t)
 /* for Fortran */
 SHMEM_TYPED_PUT(complexf, COMPLEXIFY(float))
 SHMEM_TYPED_PUT(complexd, COMPLEXIFY(double))
@@ -84,6 +131,58 @@ shmem_putmem(void *dest, const void *src,
     shmemc_put(dest, src, nelems, pe);
 }
 
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_float_p = pshmem_float_p
+#define shmem_float_p pshmem_float_p
+#pragma weak shmem_double_p = pshmem_double_p
+#define shmem_double_p pshmem_double_p
+#pragma weak shmem_longdouble_p = pshmem_longdouble_p
+#define shmem_longdouble_p pshmem_longdouble_p
+#pragma weak shmem_char_p = pshmem_char_p
+#define shmem_char_p pshmem_char_p
+#pragma weak shmem_schar_p = pshmem_schar_p
+#define shmem_schar_p pshmem_schar_p
+#pragma weak shmem_short_p = pshmem_short_p
+#define shmem_short_p pshmem_short_p
+#pragma weak shmem_int_p = pshmem_int_p
+#define shmem_int_p pshmem_int_p
+#pragma weak shmem_long_p = pshmem_long_p
+#define shmem_long_p pshmem_long_p
+#pragma weak shmem_longlong_p = pshmem_longlong_p
+#define shmem_longlong_p pshmem_longlong_p
+#pragma weak shmem_uchar_p = pshmem_uchar_p
+#define shmem_uchar_p pshmem_uchar_p
+#pragma weak shmem_ushort_p = pshmem_ushort_p
+#define shmem_ushort_p pshmem_ushort_p
+#pragma weak shmem_uint_p = pshmem_uint_p
+#define shmem_uint_p pshmem_uint_p
+#pragma weak shmem_ulong_p = pshmem_ulong_p
+#define shmem_ulong_p pshmem_ulong_p
+#pragma weak shmem_ulonglong_p = pshmem_ulonglong_p
+#define shmem_ulonglong_p pshmem_ulonglong_p
+#pragma weak shmem_int8_p = pshmem_int8_p
+#define shmem_int8_p pshmem_int8_p
+#pragma weak shmem_int16_p = pshmem_int16_p
+#define shmem_int16_p pshmem_int16_p
+#pragma weak shmem_int32_p = pshmem_int32_p
+#define shmem_int32_p pshmem_int32_p
+#pragma weak shmem_int64_p = pshmem_int64_p
+#define shmem_int64_p pshmem_int64_p
+#pragma weak shmem_uint8_p = pshmem_uint8_p
+#define shmem_uint8_p pshmem_uint8_p
+#pragma weak shmem_uint16_p = pshmem_uint16_p
+#define shmem_uint16_p pshmem_uint16_p
+#pragma weak shmem_uint32_p = pshmem_uint32_p
+#define shmem_uint32_p pshmem_uint32_p
+#pragma weak shmem_uint64_p = pshmem_uint64_p
+#define shmem_uint64_p pshmem_uint64_p
+/* for Fortran */
+#pragma weak shmem_complexf_p = pshmem_complexf_p
+#define shmem_complexf_p pshmem_complexf_p
+#pragma weak shmem_complexd_p = pshmem_complexd_p
+#define shmem_complexd_p pshmem_complexd_p
+#endif /* ENABLE_PSHMEM */
+
 #define SHMEM_TYPED_P_WRAPPER(_name, _type)                          \
     void                                                             \
     shmem_##_name##_p(_type *addr, _type val, int pe)                \
@@ -91,14 +190,30 @@ shmem_putmem(void *dest, const void *src,
         shmemc_put(addr, &val, sizeof(val), pe);                     \
     }
 
+SHMEM_TYPED_P_WRAPPER(float, float)
+SHMEM_TYPED_P_WRAPPER(double, double)
+SHMEM_TYPED_P_WRAPPER(longdouble, long double)
 SHMEM_TYPED_P_WRAPPER(char, char)
+SHMEM_TYPED_P_WRAPPER(schar, signed char)
 SHMEM_TYPED_P_WRAPPER(short, short)
 SHMEM_TYPED_P_WRAPPER(int, int)
 SHMEM_TYPED_P_WRAPPER(long, long)
 SHMEM_TYPED_P_WRAPPER(longlong, long long)
-SHMEM_TYPED_P_WRAPPER(float, float)
-SHMEM_TYPED_P_WRAPPER(double, double)
-SHMEM_TYPED_P_WRAPPER(longdouble, long double)
+SHMEM_TYPED_P_WRAPPER(uchar, unsigned char)
+SHMEM_TYPED_P_WRAPPER(ushort, unsigned short)
+SHMEM_TYPED_P_WRAPPER(uint, unsigned int)
+SHMEM_TYPED_P_WRAPPER(ulong, unsigned long)
+SHMEM_TYPED_P_WRAPPER(ulonglong, unsigned long long)
+SHMEM_TYPED_P_WRAPPER(int8, int8_t)
+SHMEM_TYPED_P_WRAPPER(int16, int16_t)
+SHMEM_TYPED_P_WRAPPER(int32, int32_t)
+SHMEM_TYPED_P_WRAPPER(int64, int64_t)
+SHMEM_TYPED_P_WRAPPER(uint8, uint8_t)
+SHMEM_TYPED_P_WRAPPER(uint16, uint16_t)
+SHMEM_TYPED_P_WRAPPER(uint32, uint32_t)
+SHMEM_TYPED_P_WRAPPER(uint64, uint64_t)
+SHMEM_TYPED_P_WRAPPER(size, size_t)
+SHMEM_TYPED_P_WRAPPER(ptrdiff, ptrdiff_t)
 /* for Fortran */
 SHMEM_TYPED_P_WRAPPER(complexf, COMPLEXIFY(float))
 SHMEM_TYPED_P_WRAPPER(complexd, COMPLEXIFY(double))
