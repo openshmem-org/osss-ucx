819f336ad216e28c1025ef478bb0cf78116ad405
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-11-01 19:08:55 -0400

    Merge barrier & sync in API as for shmemc

--- src/barrier.c
+++ src/barrier.c
@@ -25,3 +25,24 @@ shmem_barrier_all(void)
 {
     shmemc_barrier_all();
 }
+
+
+#ifdef ENABLE_PSHMEM
+#pragma weak shmem_sync_all = pshmem_sync_all
+#define shmem_sync_all pshmem_sync_all
+
+#pragma weak shmem_sync = pshmem_sync
+#define shmem_sync pshmem_sync
+#endif /* ENABLE_PSHMEM */
+
+void
+shmem_sync(int start, int log2stride, int size, long *pSync)
+{
+    shmemc_sync(start, log2stride, size, pSync);
+}
+
+void
+shmem_sync_all(void)
+{
+    shmemc_sync_all();
+}
