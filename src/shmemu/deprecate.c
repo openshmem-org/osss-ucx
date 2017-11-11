c00f00c32d4ae39b751c2233430c5e906f4239dc
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-11-11 11:24:49 -0500

    Move the uthash code away from visible API

--- src/shmemu/deprecate.c
+++ src/shmemu/deprecate.c
@@ -7,6 +7,7 @@
 #ifdef ENABLE_DEBUG
 
 #include "shmemu.h"
+
 #include "uthash.h"
 
 #include <stdlib.h>
@@ -17,42 +18,56 @@ struct depr {
     UT_hash_handle hh;
 };
 
-static struct depr *table;
+static struct depr *table = NULL;
+
+inline static int
+already_seen(const char *name)
+{
+    struct depr *lu = NULL;
+
+    HASH_FIND_STR(table, name, lu);
+
+    return (lu != NULL) ? 1 : 0;
+}
+
+inline static void
+record(const char *name)
+{
+    struct depr *dp = (struct depr *) malloc(sizeof(*dp));
+
+    assert(dp != NULL);
+
+    dp->name = strdup(name);
+
+    HASH_ADD_STR(table, name, dp);
+}
 
 /*
  * need to restrict this report to first usage only
  */
 void
-shmemu_deprecate(const char *fn)
+shmemu_deprecate(const char *fn_name)
 {
-    struct depr *lu;
-
-    HASH_FIND_STR(table, fn, lu);
-
-    if (lu == NULL) {
-        struct depr *dp = (struct depr *) malloc(sizeof(*dp));
-
-        assert(dp != NULL);
-
-        dp->name = strdup(fn);
-
-        HASH_ADD_STR(table, name, dp);
-
-        logger(LOG_DEPRECATE, "\"%s\" is deprecated", fn);
+    if (already_seen(fn_name)) {
+        return;
     }
+
+    record(fn_name);
+
+    logger(LOG_DEPRECATE, "\"%s\" is deprecated", fn_name);
 }
 
 void
 shmemu_deprecate_init(void)
 {
-    table = NULL;
+    return;
 }
 
 void
 shmemu_deprecate_finalize(void)
 {
-    struct depr *cur;
-    struct depr *tmp;
+    struct depr *cur = NULL;
+    struct depr *tmp = NULL;
 
     HASH_ITER(hh, table, cur, tmp) {
         free(cur->name);   /* was strdup'ed above */
