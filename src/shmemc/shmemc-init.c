9125f30351f3c6215e55bf2f9c06e4c957b9ece8
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-11-13 16:44:29 -0500

    Rework startup with PMIx, looks to be race-conditions that don't show up locally.

--- src/shmemc/shmemc-init.c
+++ src/shmemc/shmemc-init.c
@@ -6,11 +6,8 @@
 
 #include "shmemc-ucx.h"
 #include "pmix-client.h"
-#include "state.h"
 #include "shmemu.h"
 #include "shmemc.h"
-
-#include "shmem/defs.h"
 
 void
 shmemc_init(void)
@@ -24,18 +21,17 @@ shmemc_init(void)
     /* now heap registered... */
     /* TODO: don't do this if all addresses fixed */
     shmemc_pmix_publish_heap_info();
-    shmemc_pmix_exchange_heap_info();
-
-    /* exchange worker info and then create EPs */
     shmemc_pmix_publish_worker();
-    shmemc_pmix_exchange_workers();
+    shmemc_pmix_barrier_all();
 
+    shmemc_pmix_exchange_heap_info();
+    /* exchange worker info and then create EPs */
+    shmemc_pmix_exchange_workers();
     shmemc_ucx_make_remote_endpoints();
 
     shmemc_pmix_publish_my_rkeys();
+    shmemc_pmix_barrier_all();
     shmemc_pmix_exchange_all_rkeys();
-
-    shmemc_pmix_client_finalize();
 
 #if 0
     shmemc_barrier_all();
@@ -46,6 +42,9 @@ void
 shmemc_finalize(void)
 {
     shmemc_barrier_all();       /* finalize has implicit global barrier */
+
+    /* shmemc_pmix_barrier_all(); */
+    shmemc_pmix_client_finalize();
 
     shmemc_ucx_finalize();
 }
