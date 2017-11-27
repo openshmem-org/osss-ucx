6d61076489e3ad60f90ff357b6278795998a1aaf
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-11-27 11:06:32 -0500

    move memory region allocation to right place

--- src/shmemc/pmix-client.c
+++ src/shmemc/pmix-client.c
@@ -264,31 +264,6 @@ shmemc_pmix_exchange_all_rkeys(void)
     }
 }
 
-/*
- * TODO: I am aware this is in the wrong place
- */
-
-static void
-init_regions(void)
-{
-    size_t i;
-
-    /* TODO: hardwire for now: globals + default symmetric heap */
-    proc.comms.nregions = 2;
-
-    /* init that many regions on me */
-    proc.comms.regions =
-        (mem_region_t *) malloc(proc.comms.nregions * sizeof(mem_region_t));
-    assert(proc.comms.regions != NULL);
-
-    /* now prep for all PEs to exchange */
-    for (i = 0; i < proc.comms.nregions; i += 1) {
-        proc.comms.regions[i].minfo =
-            (mem_info_t *) malloc(proc.nranks * sizeof(mem_info_t));
-        assert(proc.comms.regions[i].minfo != NULL);
-    }
-}
-
 /* -------------------------------------------------------------- */
 
 /*
@@ -346,7 +321,7 @@ shmemc_pmix_client_init(void)
 
     parse_peers(vp->data.string);
 
-    init_regions();
+    // init_regions();
 
     /* and done */
     PMIX_VALUE_RELEASE(vp);
