73e769e5c1b4d0137d6645620357491d2db617ec
Tony Curtis <anthony.curtis@stonybrook.edu>
2017-12-09 16:16:55 -0500

    Check for threading configuration, compile out mutex if not there

--- src/shmem_mutex.h
+++ src/shmem_mutex.h
@@ -3,11 +3,17 @@
 #ifndef _SHMEM_MUTEX_H
 #define _SHMEM_MUTEX_H 1
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
 #include "state.h"
 
 #include "shmem/defs.h"
 
 #include <pthread.h>
+
+#ifdef ENABLE_THREADS
 
 inline static void
 shmeml_mutex_init(pthread_mutex_t *mp)
@@ -47,5 +53,16 @@ shmeml_mutex_unlock(pthread_mutex_t *mp)
         _fn;                                      \
         shmeml_mutex_unlock(_mp);                 \
     } while (0)
+
+#else
+
+#define shmeml_mutex_init(_mp)
+#define shmeml_mutex_destroy(_mp)
+#define shmeml_mutex_lock(_mp)
+#define shmeml_mutex_unlock(_mp)
+
+#define SHMEML_MUTEX_PROTECT(_fn, _mp) _fn
+
+#endif  /* ENABLE_THREADS */
 
 #endif /* ! _SHMEM_MUTEX_H */
