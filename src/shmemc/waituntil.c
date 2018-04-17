852f546c5ba0c5d07bfafd42b2bcfe9e87750729
Tony Curtis <anthony.curtis@stonybrook.edu>
2018-04-17 15:52:46 -0400

    Wait-on-memory-change accidentally misplaced.
    Was in test routine instead of wait.

--- src/shmemc/waituntil.c
+++ src/shmemc/waituntil.c
@@ -25,9 +25,6 @@
                                      int##_size##_t *var,               \
                                      int##_size##_t value)              \
     {                                                                   \
-        shmemc_context_h ch = (shmemc_context_h) ctx;                   \
-                                                                        \
-        ucp_worker_wait_mem(ch->w, var);                                \
         return ( (*var) _op (value) ) ? 1 : 0;                          \
     }
 
@@ -61,8 +58,11 @@ COMMS_CTX_TEST_SIZE(64, ge, >=)
                                              int##_size##_t *var,       \
                                              int##_size##_t value)      \
     {                                                                   \
+        shmemc_context_h ch = (shmemc_context_h) ctx;                   \
+                                                                        \
         do {                                                            \
             shmemc_progress();                                          \
+            ucp_worker_wait_mem(ch->w, var);                            \
         } while (shmemc_ctx_test_##_opname##_size(ctx, var, value) == 0); \
     }
 
