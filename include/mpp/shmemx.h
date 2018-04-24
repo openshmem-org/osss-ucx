e289bede86d549ce7602673a2d470db17ace9a93
Tony Curtis <anthony.curtis@stonybrook.edu>
2018-04-24 15:12:37 -0400

    Show spec. level for deprecation of mpp/ header files (C)

--- include/mpp/shmemx.h
+++ include/mpp/shmemx.h
@@ -3,7 +3,7 @@
 #ifndef _MPP_SHMEMX_H
 #define _MPP_SHMEMX_H 1
 
-#warning "<mpp/shmemx.h> is a deprecated location, use <shmemx.h> instead"
+#warning "<mpp/shmemx.h> is a deprecated location since specification 1.1, use <shmemx.h> instead"
 
 #include <shmemx.h>
 
