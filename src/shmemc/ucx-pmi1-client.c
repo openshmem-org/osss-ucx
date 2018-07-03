d8e890c5f72735631b780e8799ed52ffd9c1aef6
Tony Curtis <anthony.curtis@stonybrook.edu>
2018-07-03 12:39:07 -0400

    Flesh out fallback PMI1 client code

--- src/shmemc/ucx-pmi1-client.c
+++ src/shmemc/ucx-pmi1-client.c
@@ -17,108 +17,259 @@
 
 #include <ucp/api/ucp.h>
 
-static char *kvs_name = NULL;   /* PMI namespace */
-static int kvs_max_len;
-
-inline static char *
-alloc_string(void)
-{
-    return (char *) malloc(sizeof(*buf) * kvs_max_len);
-}
-
-#ifndef ENABLE_ALIGNED_ADDRESSES
+/* -------------------------------------------------------------- */
 
 /*
- * formats are <pe>:<region-index>:region:<key>
+ * rotate/spread PE communications
  */
-static const char *region_base_fmt = "%d:%d:mr:base";
-static const char *region_size_fmt = "%d:%d:mr:size";
+#define SHIFT(_pe) ( ((_pe) + proc.rank) % proc.nranks )
 
-void
-shmemc_pmi_publish_heap_info(void)
+/*
+ * Persistent local state
+ */
+
+static char *kvs_name = NULL;   /* PMI namespace */
+
+static int kvs_max_name_len;
+static int kvs_max_key_len;
+static int kvs_max_value_len;
+
+/*
+ * allocate state variables
+ */
+
+inline static char *
+alloc_string(size_t n)
 {
+    return (char *) malloc(n);
 }
 
-void
-shmemc_pmi_exchange_heap_info(void)
-{
-}
+/* -------------------------------------------------------------- */
 
-#endif /* ! ENABLE_ALIGNED_ADDRESSES */
-
-static const char *wrkr_exch_fmt = "%d:wrkr:addr";
+static const char *wrkr_exch_fmt = "%d:wrkr:addr"; /* pe */
 
 /*
  * PMI key/val lookups
  */
-static char *key;
-static char *val;
+static char *key = NULL;
+static char *val = NULL;
 
 void
 shmemc_pmi_publish_worker(void)
 {
-    int next;
+    int ps;
 
     /* everyone publishes their info */
-    snprintf(key, kvs_max_len,
+    snprintf(key, kvs_max_key_len,
              wrkr_exch_fmt,
              proc.rank);
-    next = snprint(val, "%8lu", proc.comms.xchg_wrkr_info[proc.rank].len);
-    memcpy(val + next,
-           proc.comms.xchg_wrkr_info[proc.rank].addr,
-           proc.comms.xchg_wrkr_info[proc.rank].len);
+    memcpy(val,
+           & proc.comms.xchg_wrkr_info[proc.rank],
+           sizeof(proc.comms.xchg_wrkr_info[proc.rank]));
+
+    ps = PMI_KVS_Put(kvs_name, key, val);
+    shmemu_assert(ps == PMI_SUCCESS,
+                  "put of worker blob failed (status %d)",
+                  ps);
+}
+
+static const char *rkey_exch_fmt   = "rkey:%lu:%d"; /* region, pe */
+
+#ifndef ENABLE_ALIGNED_ADDRESSES
+static const char *heap_exch_fmt = "heap:%lu:%d"; /* region, pe */
+#endif /* ! ENABLE_ALIGNED_ADDRESSES */
+
+inline static void
+publish_one_rkeys(size_t r)
+{
+    int ps;
+    int next;
+    void *packed_rkey;
+    size_t rkey_len;
+    const ucs_status_t s =
+        ucp_rkey_pack(proc.comms.ucx_ctxt,
+                      proc.comms.regions[r].minfo[proc.rank].racc.mh,
+                      &packed_rkey, &rkey_len
+                      );
+    shmemu_assert(s == UCS_OK, "can't pack rkey");
+
+    snprintf(key, kvs_max_key_len, rkey_exch_fmt, proc.rank);
+
+    next = snprintf(val, kvs_max_value_len, "%8lu", rkey_len);
+    memcpy(val + next, packed_rkey, rkey_len);
 
     ps = PMI_KVS_Put(kvs_name, key, val);
     shmemu_assert(ps == PMI_SUCCESS,
                   "put of worker blob failed (status %d)",
                   ps);
 
-    ps = PMI_KVS_Commit(kvs_name);
+    ucp_rkey_buffer_release(packed_rkey);
+}
+
+#ifndef ENABLE_ALIGNED_ADDRESSES
+inline static void
+publish_one_heap(size_t r)
+{
+    int ps;
+    const mem_info_t *mp = & proc.comms.regions[r].minfo[proc.rank];
+
+    /* everyone publishes their info */
+    snprintf(key, kvs_max_key_len,
+             heap_exch_fmt,
+             proc.rank);
+    memcpy(val, mp, sizeof(*mp));
+
+    ps = PMI_KVS_Put(kvs_name, key, val);
     shmemu_assert(ps == PMI_SUCCESS,
-                  "worker blob commit failed (status %d)",
+                  "put of heap blob failed (status %d)",
                   ps);
+}
+#endif /* ! ENABLE_ALIGNED_ADDRESSES */
+
+void
+shmemc_pmi_publish_rkeys_and_heaps(void)
+{
+    size_t r;
+
+    publish_one_rkeys(0);
+
+    for (r = 1; r < proc.comms.nregions; r += 1) {
+        publish_one_rkeys(r);
+
+#ifndef ENABLE_ALIGNED_ADDRESSES
+        publish_one_heap(r);
+#endif /* ! ENABLE_ALIGNED_ADDRESSES */
+    }
 }
 
 void
 shmemc_pmi_exchange_workers(void)
 {
     int pe;
-    unsigned long len;
 
     for (pe = 0; pe < proc.nranks; pe += 1) {
-        const int i = (pe + proc.rank) % proc.nranks;
+        int ps;
+        worker_info_t *wp;
+        const int i = SHIFT(pe);
 
-        snprintf(key, kvx_max_len,
-                 "%d:wrkr:addr",
+        snprintf(key, kvs_max_key_len,
+                 wrkr_exch_fmt,
                  i);
 
-        ps = PMI_KVS_Get(kvs_name, key, val, kvs_max_len);
+        ps = PMI_KVS_Get(kvs_name, key, val, kvs_max_value_len);
         shmemu_assert(ps == PMI_SUCCESS,
                       "get of remote worker blob failed (status %d)",
                       ps);
 
-        sscanf(val, "%8lu", &len);
+        wp = (worker_info_t *) val;
 
-        proc.comms.xchg_wrkr_info[i].buf = (char *) malloc(len);
+        proc.comms.xchg_wrkr_info[i].buf = (char *) malloc(wp->len);
         shmemu_assert(proc.comms.xchg_wrkr_info[i].buf != NULL,
                       "can't allocate memory for remote worker blob");
-        memcpy(proc.comms.xchg_wrkr_info[i].buf, val + 8, len);
+        memcpy(proc.comms.xchg_wrkr_info[i].buf, wp->addr, wp->len);
     }
 }
 
-/*
- * PE:rkey:HEAPNO
- */
-static const char *rkey_exch_fmt = "%d:rkey:%d";
-
-void
-shmemc_pmi_publish_my_rkeys(void)
+inline static void
+exchange_one_rkeys(size_t r, int pe)
 {
+    const int i = SHIFT(pe);
+    int ps;
+    ucs_status_t s;
+    size_t len;
+
+    snprintf(key, kvs_max_key_len,
+             rkey_exch_fmt,
+             r, i);
+
+    ps = PMI_KVS_Get(kvs_name, key, val, kvs_max_value_len);
+    shmemu_assert(ps == PMI_SUCCESS,
+                  "fetch of rkey from PE %d failed (status %d)",
+                  i, ps);
+
+    sscanf(val, "%8lu", &len);
+
+    proc.comms.regions[r].minfo[i].racc.rkey =
+        (ucp_rkey_h) malloc(len);
+    shmemu_assert(proc.comms.regions[r].minfo[i].racc.rkey != NULL,
+                  "can't allocate memory for remote rkey");
+
+    s = ucp_ep_rkey_unpack(proc.comms.eps[i],
+                           val + len,
+                           &proc.comms.regions[r].minfo[i].racc.rkey
+                           );
+    shmemu_assert(s == UCS_OK, "can't unpack remote rkey");
+}
+
+#ifndef ENABLE_ALIGNED_ADDRESSES
+inline static void
+exchange_one_heap(size_t r, int pe)
+{
+    int ps;
+    mem_info_t *mp;
+
+    snprintf(key, kvs_max_key_len,
+             heap_exch_fmt,
+             r, pe);
+
+    ps = PMI_KVS_Get(kvs_name, key, val, kvs_max_value_len);
+    shmemu_assert(ps == PMI_SUCCESS,
+                  "fetch of rkey from PE %d failed (status %d)",
+                  pe, ps);
+
+    mp = (mem_info_t *) val;
+
+    proc.comms.regions[r].minfo[pe].base =
+        mp->base;
+    proc.comms.regions[r].minfo[pe].len =
+        mp->len;
+    /* slightly redundant storage, but useful */
+    proc.comms.regions[r].minfo[pe].end =
+        proc.comms.regions[r].minfo[pe].base +
+        mp->len;
+}
+#endif /* ! ENABLE_ALIGNED_ADDRESSES */
+
+inline static void
+exchange_all_rkeys(size_t r)
+{
+    int pe;
+
+    for (pe = 0; pe < proc.nranks; pe += 1) {
+        const int i = SHIFT(pe);
+
+        exchange_one_rkeys(r, i);
+    }
+}
+
+inline static void
+exchange_one_rkeys_and_heaps(size_t r)
+{
+    int pe;
+
+    for (pe = 0; pe < proc.nranks; pe += 1) {
+        const int i = SHIFT(pe);
+
+        exchange_one_rkeys(r, i);
+
+#ifndef ENABLE_ALIGNED_ADDRESSES
+        exchange_one_heap(r, i);
+#endif /* ! ENABLE_ALIGNED_ADDRESSES */
+    }  /* PE loop */
 }
 
 void
-shmemc_pmi_exchange_all_rkeys(void)
+shmemc_pmi_exchange_rkeys_and_heaps(void)
 {
+    size_t r;
+
+    /* global rkeys */
+    exchange_all_rkeys(0);
+
+    /* now everything else */
+    for (r = 1; r < proc.comms.nregions; r += 1) {
+        exchange_one_rkeys_and_heaps(r);
+    }
 }
 
 /* -------------------------------------------------------------- */
@@ -130,8 +281,14 @@ shmemc_pmi_exchange_all_rkeys(void)
 void
 shmemc_pmi_barrier_all(void)
 {
-    const int ps = PMI_Barrier();
+    int ps;
 
+    ps = PMI_KVS_Commit(kvs_name);
+    shmemu_assert(ps == PMI_SUCCESS,
+                  "worker blob commit failed (status %d)",
+                  ps);
+
+    ps = PMI_Barrier();
     shmemu_assert(ps == PMI_SUCCESS,
                   "PMI barrier failed (status %d)",
                   ps);
@@ -174,27 +331,37 @@ shmemc_pmi_client_init(void)
                   "PMI PE rank %d is not valid",
                   proc.rank);
 
-    /* I have no peer info */
+    /* I have no peer info (at present) */
     proc.npeers = 0;
     proc.peers = NULL;
 
-    ps = PMI_KVS_Get_key_length_max(&kvs_max_len);
+    ps = PMI_KVS_Get_name_length_max(&kvs_max_name_len);
+    shmemu_assert(ps == PMI_SUCCESS,
+                  "PMI can't get max name length (status %d)",
+                  ps);
+
+    ps = PMI_KVS_Get_key_length_max(&kvs_max_key_len);
     shmemu_assert(ps == PMI_SUCCESS,
                   "PMI can't get max key length (status %d)",
                   ps);
 
-    kvs_name = alloc_string();
+    ps = PMI_KVS_Get_value_length_max(&kvs_max_value_len);
+    shmemu_assert(ps == PMI_SUCCESS,
+                  "PMI can't get max value length (status %d)",
+                  ps);
+
+    kvs_name = alloc_string(kvs_max_name_len);
     shmemu_assert(kvs_name != NULL, "PMI can't allocate name string");
 
-    ps = PMI_KVS_Get_my_name(kvs_name, kvs_max_len);
+    ps = PMI_KVS_Get_my_name(kvs_name, kvs_max_name_len);
     shmemu_assert(ps == PMI_SUCCESS,
                   "PMI can't find its name (status %d)",
                   ps);
 
-    key = alloc_string();
+    key = alloc_string(kvs_max_key_len);
     shmemu_assert(key != NULL,
                   "PMI can't allocate memory for key holder");
-    val = alloc_string();
+    val = alloc_string(kvs_max_value_len);
     shmemu_assert(val != NULL,
                   "PMI can't allocate memory for value holder");
 }
@@ -222,7 +389,7 @@ shmemc_pmi_client_finalize(void)
         }
     }
 
-    /* clean up allocated memory */
+    /* clean up locally allocated memory */
     free(proc.peers);
     free(kvs_name);
     free(key);
