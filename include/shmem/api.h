2052eb72f15a72e6eb5494df6fd808b3aecbd7d6
Tony Curtis <anthony.curtis@stonybrook.edu>
2019-10-15 10:24:25 -0400

    Fix signature of signal_fetch

--- include/shmem/api.h
+++ include/shmem/api.h
@@ -861,26 +861,25 @@ extern "C"
 #undef API_DECL_PUTMEM_SIGNAL
 
     /**
-     * @brief fetches value of the signl object.
+     * @brief fetches value of the signal object.
      * @page shmem_signel_fetch
      * @section Synopsis
      *
      * @subsection c C/C++
      @code
-     uint64_t shmem_signal_fetch(const uint64_t *sig_addr, int pe);
+     uint64_t shmem_signal_fetch(const uint64_t *sig_addr);
      @endcode
      *
      * @param[in] sig_addr The address of the remotely accessible signal object
-     * @param[in] pe The PE to fetch from
      *
      * @section Effect
      * None.
      *
      * @section Return
-     * The remote data value.
+     * The data value.
      *
      */
-    uint64_t shmem_signal_fetch(const uint64_t *sig_addr, int pe);
+    uint64_t shmem_signal_fetch(const uint64_t *sig_addr);
 
     /*
      * barriers & syncs
