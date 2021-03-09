769a8d53d1d11498914b4b0c63787b8e36ca92d8
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-03-09 08:42:40 -0500

    Missing deprecation text

--- include/mpp/shmem.h
+++ include/mpp/shmem.h
@@ -3,7 +3,7 @@
 #ifndef _MPP_SHMEM_H
 #define _MPP_SHMEM_H 1
 
-#warning "<mpp/shmem.h>: with specification 1.1 and up, use <shmem.h> instead"
+#warning "<mpp/shmem.h> is deprecated: with specification 1.1 and up, use <shmem.h> instead"
 
 #include <shmem.h>
 
