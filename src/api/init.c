37437dab598eaffa1a7bac6144d00896c16c973a
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-06-25 15:03:30 -0400

    Short-circuit pe lookup

--- src/api/init.c
+++ src/api/init.c
@@ -141,7 +141,7 @@ init_thread_helper(int requested, int *provided)
 
     ++proc.refcount;
 
-    if (shmemc_my_pe() == 0) {
+    if (proc.li.rank == 0) {
         if (proc.env.print_version) {
             info_output_package_version(stdout, "# ", 0);
         }
