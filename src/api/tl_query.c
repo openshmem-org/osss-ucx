9cddd8d375169097e4fdf1f9812da7f9ee27a53e
Tony Curtis <anthony.curtis@stonybrook.edu>
2021-11-24 22:15:21 -0500

    Clean up thread-level query

--- src/api/tl_query.c
+++ src/api/tl_query.c
@@ -6,7 +6,6 @@
 
 #include "shmemu.h"
 #include "state.h"
-#include "shmem/api.h"
 
 #ifdef ENABLE_PSHMEM
 #pragma weak shmem_query_thread = pshmem_query_thread
@@ -23,11 +22,7 @@ shmem_query_thread(int *provided)
     SHMEMU_CHECK_INIT();
     SHMEMU_CHECK_NOT_NULL(provided, 1);
 
-    logger(LOG_INFO,
-           "%s() -> %d",
-           __func__,
-           proc.td.osh_tl
-           );
+    logger(LOG_INFO, "%s() -> %d", __func__, proc.td.osh_tl);
 
     *provided = proc.td.osh_tl;
 }
